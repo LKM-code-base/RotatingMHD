fcaf3c9dc1d348cc541620242b5e086f97594a46
Jose <jose.lara-hernandez@bam.de>
2020-07-25 15:26:34 +0200

    Created new methodsfor better organization

--- source/projection_solver/problem_setup.cc
+++ source/projection_solver/problem_setup.cc
@@ -5,123 +5,141 @@
 
 namespace Step35
 {
-  template <int dim>
-  void NavierStokesProjection<dim>::
-  initialize()
+template <int dim>
+void NavierStokesProjection<dim>::
+setup_matrices_and_vectors()
+{
+  setup_velocity_matrices();
+  setup_pressure_matrices();
+  setup_pressure_gradient_matrix();
+
+  pressure_n.reinit(pressure_dof_handler.n_dofs());
+  pressure_n_minus_1.reinit(pressure_dof_handler.n_dofs());
+  pressure_rhs.reinit(pressure_dof_handler.n_dofs());
+  pressure_tmp.reinit(pressure_dof_handler.n_dofs());
+  phi_n.reinit(pressure_dof_handler.n_dofs());
+  phi_n_minus_1.reinit(pressure_dof_handler.n_dofs());
+  velocity_n.reinit(velocity_dof_handler.n_dofs());
+  velocity_n_minus_1.reinit(velocity_dof_handler.n_dofs());
+  extrapolated_velocity.reinit(velocity_dof_handler.n_dofs());
+  velocity_rhs.reinit(velocity_dof_handler.n_dofs());
+  velocity_tmp.reinit(velocity_dof_handler.n_dofs());
+}
+
+template <int dim>
+void NavierStokesProjection<dim>::
+assemble_constant_matrices()
+{
+  assemble_velocity_matrices();
+  assemble_pressure_matrices();
+  assemble_pressure_gradient_matrix();
+
+  if (!flag_adpative_time_step)
   {
-    setup_v_matrices();
-    setup_p_matrices();
-    setup_p_gradient_matrix();
-
-    p_n.reinit(p_dof_handler.n_dofs());
-    p_n_m1.reinit(p_dof_handler.n_dofs());
-    p_rhs.reinit(p_dof_handler.n_dofs());
-    p_tmp.reinit(p_dof_handler.n_dofs());
-    phi_n.reinit(p_dof_handler.n_dofs());
-    phi_n_m1.reinit(p_dof_handler.n_dofs());
-    v_n.reinit(v_dof_handler.n_dofs());
-    v_n_m1.reinit(v_dof_handler.n_dofs());
-    v_extrapolated.reinit(v_dof_handler.n_dofs());
-    v_rhs.reinit(v_dof_handler.n_dofs());
-    v_tmp.reinit(v_dof_handler.n_dofs());
-
-    assemble_v_matrices();
-    assemble_p_matrices();
-    assemble_p_gradient_matrix();
-
-    if (!flag_adpative_time_step)
-    {
-      v_mass_plus_laplace_matrix = 0.;
-      v_mass_plus_laplace_matrix.add(1.0 / Re, v_laplace_matrix);
-      v_mass_plus_laplace_matrix.add(1.5 / dt_n, v_mass_matrix);
-    }
-
-    phi_n     = 0.;
-    phi_n_m1  = 0.;
-
-    p_initial_conditions.set_time(t_0);    
-    VectorTools::interpolate(p_dof_handler, 
-                             p_initial_conditions, 
-                             p_n_m1);
-    p_initial_conditions.advance_time(dt_n);
-    VectorTools::interpolate(p_dof_handler, 
-                             p_initial_conditions, 
-                             p_n);
-
-    v_initial_conditions.set_time(t_0);    
-    VectorTools::interpolate(v_dof_handler,
-                             v_initial_conditions,
-                             v_n_m1);
-    v_initial_conditions.advance_time(dt_n);
-    VectorTools::interpolate(v_dof_handler,
-                             v_initial_conditions,
-                             v_n);
-  }
-
-  template <int dim>
-  void NavierStokesProjection<dim>::
-  setup_v_matrices()
-  {
-    {
-      DynamicSparsityPattern dsp(v_dof_handler.n_dofs(),
-                                 v_dof_handler.n_dofs());
-      DoFTools::make_sparsity_pattern(v_dof_handler, 
-                                      dsp,
-                                      v_constraints);
-      v_sparsity_pattern.copy_from(dsp);
-      std::ofstream out ("v_sparsity_pattern.gpl");
-      v_sparsity_pattern.print_gnuplot(out);
-    }
-    v_mass_plus_laplace_matrix.reinit(v_sparsity_pattern);
-    v_system_matrix.reinit(v_sparsity_pattern);
-    v_mass_matrix.reinit(v_sparsity_pattern);
-    v_laplace_matrix.reinit(v_sparsity_pattern);
-    v_advection_matrix.reinit(v_sparsity_pattern);
-  }
-
-  template <int dim>
-  void NavierStokesProjection<dim>::
-  setup_p_matrices()
-  {
-    {
-      DynamicSparsityPattern dsp(p_dof_handler.n_dofs(),
-                                 p_dof_handler.n_dofs());
-      DoFTools::make_sparsity_pattern(p_dof_handler, 
-                                      dsp,
-                                      p_constraints);
-      p_sparsity_pattern.copy_from(dsp);
-      std::ofstream out ("p_sparsity_pattern.gpl");
-      p_sparsity_pattern.print_gnuplot(out);
-    }
-
-    p_laplace_matrix.reinit(p_sparsity_pattern);
-    p_system_matrix.reinit(p_sparsity_pattern);
-    p_mass_matrix.reinit(p_sparsity_pattern);
-  }
-
-  template <int dim>
-  void NavierStokesProjection<dim>::
-  setup_p_gradient_matrix()
-  {
-    {
-      DynamicSparsityPattern dsp(v_dof_handler.n_dofs(),
-                                 p_dof_handler.n_dofs());
-      DoFTools::make_sparsity_pattern(v_dof_handler,
-                                      p_dof_handler,
-                                      dsp);
-      mixed_sparsity_pattern.copy_from(dsp);
-      std::ofstream out("mixed_sparsity_pattern.gpl");
-      mixed_sparsity_pattern.print_gnuplot(out);
-    }
-    p_gradient_matrix.reinit(mixed_sparsity_pattern);
+    velocity_mass_plus_laplace_matrix = 0.;
+    velocity_mass_plus_laplace_matrix.add(1.0 / Re, 
+                                          velocity_laplace_matrix);
+    velocity_mass_plus_laplace_matrix.add(1.5 / dt_n, 
+                                          velocity_mass_matrix);
   }
 }
 
+template <int dim>
+void NavierStokesProjection<dim>::
+setup_velocity_matrices()
+{
+  {
+    DynamicSparsityPattern dsp(velocity_dof_handler.n_dofs(),
+                                velocity_dof_handler.n_dofs());
+    DoFTools::make_sparsity_pattern(velocity_dof_handler, 
+                                    dsp,
+                                    velocity_constraints);
+    velocity_sparsity_pattern.copy_from(dsp);
+    std::ofstream out ("velocity_sparsity_pattern.gpl");
+    velocity_sparsity_pattern.print_gnuplot(out);
+  }
+  velocity_mass_plus_laplace_matrix.reinit(velocity_sparsity_pattern);
+  velocity_system_matrix.reinit(velocity_sparsity_pattern);
+  velocity_mass_matrix.reinit(velocity_sparsity_pattern);
+  velocity_laplace_matrix.reinit(velocity_sparsity_pattern);
+  velocity_advection_matrix.reinit(velocity_sparsity_pattern);
+}
+
+template <int dim>
+void NavierStokesProjection<dim>::
+setup_pressure_matrices()
+{
+  {
+    DynamicSparsityPattern dsp(pressure_dof_handler.n_dofs(),
+                                pressure_dof_handler.n_dofs());
+    DoFTools::make_sparsity_pattern(pressure_dof_handler, 
+                                    dsp,
+                                    pressure_constraints);
+    pressure_sparsity_pattern.copy_from(dsp);
+    std::ofstream out ("pressure_sparsity_pattern.gpl");
+    pressure_sparsity_pattern.print_gnuplot(out);
+  }
+
+  pressure_laplace_matrix.reinit(pressure_sparsity_pattern);
+  //pressure_system_matrix.reinit(pressure_sparsity_pattern);
+  pressure_mass_matrix.reinit(pressure_sparsity_pattern);
+}
+
+template <int dim>
+void NavierStokesProjection<dim>::
+setup_pressure_gradient_matrix()
+{
+  {
+    DynamicSparsityPattern dsp(velocity_dof_handler.n_dofs(),
+                                pressure_dof_handler.n_dofs());
+    DoFTools::make_sparsity_pattern(velocity_dof_handler,
+                                    pressure_dof_handler,
+                                    dsp);
+    mixed_sparsity_pattern.copy_from(dsp);
+    std::ofstream out("mixed_sparsity_pattern.gpl");
+    mixed_sparsity_pattern.print_gnuplot(out);
+  }
+  pressure_gradient_matrix.reinit(mixed_sparsity_pattern);
+}
+
+template <int dim>
+void NavierStokesProjection<dim>::
+initialize()
+{
+  phi_n         = 0.;
+  phi_n_minus_1 = 0.;
+
+  pressure_initial_conditions.set_time(t_0);    
+  VectorTools::interpolate(pressure_dof_handler, 
+                            pressure_initial_conditions, 
+                            pressure_n_minus_1);
+  pressure_initial_conditions.advance_time(dt_n);
+  VectorTools::interpolate(pressure_dof_handler, 
+                            pressure_initial_conditions, 
+                            pressure_n);
+
+  velocity_initial_conditions.set_time(t_0);    
+  VectorTools::interpolate(velocity_dof_handler,
+                            velocity_initial_conditions,
+                            velocity_n_minus_1);
+  velocity_initial_conditions.advance_time(dt_n);
+  VectorTools::interpolate(velocity_dof_handler,
+                            velocity_initial_conditions,
+                            velocity_n);
+}
+
+}
+
+// explicit instantiations
+template void Step35::NavierStokesProjection<2>::setup_matrices_and_vectors();
+template void Step35::NavierStokesProjection<3>::setup_matrices_and_vectors();
+template void Step35::NavierStokesProjection<2>::assemble_constant_matrices();
+template void Step35::NavierStokesProjection<3>::assemble_constant_matrices();
+template void Step35::NavierStokesProjection<2>::setup_velocity_matrices();
+template void Step35::NavierStokesProjection<3>::setup_velocity_matrices();
+template void Step35::NavierStokesProjection<2>::setup_pressure_matrices();
+template void Step35::NavierStokesProjection<3>::setup_pressure_matrices();
+template void Step35::NavierStokesProjection<2>::setup_pressure_gradient_matrix();
+template void Step35::NavierStokesProjection<3>::setup_pressure_gradient_matrix();
 template void Step35::NavierStokesProjection<2>::initialize();
 template void Step35::NavierStokesProjection<3>::initialize();
-template void Step35::NavierStokesProjection<2>::setup_v_matrices();
-template void Step35::NavierStokesProjection<3>::setup_v_matrices();
-template void Step35::NavierStokesProjection<2>::setup_p_matrices();
-template void Step35::NavierStokesProjection<3>::setup_p_matrices();
-template void Step35::NavierStokesProjection<2>::setup_p_gradient_matrix();
-template void Step35::NavierStokesProjection<3>::setup_p_gradient_matrix();
