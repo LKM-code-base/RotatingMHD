63671f920f0db731d39d09e844d80e2938fbfc53
Jose <jose.lara-hernandez@bam.de>
2020-07-25 15:19:44 +0200

    Renamed and reordered variables. Added new methods for better organization

--- include/rotatingMHD/projection_solver.h
+++ include/rotatingMHD/projection_solver.h
@@ -33,126 +33,136 @@ namespace Step35
 
   using namespace dealii;
 
-  template <int dim>
-  class NavierStokesProjection
-  {
-  public:
-    NavierStokesProjection(const RunTimeParameters::ParameterSet &data);
-    void run(const bool flag_verbose_output = false, 
-             const unsigned int n_plots = 10);
+template <int dim>
+class NavierStokesProjection
+{
+public:
+  NavierStokesProjection(const RunTimeParameters::ParameterSet &data);
+  void run(const bool flag_verbose_output = false, 
+           const unsigned int n_plots = 10);
 
-  protected:
-    RunTimeParameters::ProjectionMethod       projection_method;
-    const unsigned int                        p_fe_degree;
-    const unsigned int                        v_fe_degree;
-    double                                    dt_n;
-    double                                    dt_n_m1;
-    const double                              t_0;
-    const double                              T;
-    const double                              Re;
+private:
+  RunTimeParameters::ProjectionMethod projection_method;
+  double                              dt_n;
+  double                              dt_n_minus_1;
+  const double                        t_0;
+  const double                        T;
+  const double                        Re;
 
-    EquationData::VelocityInflowBC<dim>       inflow_bc;
-    EquationData::VelocityIC<dim>             v_initial_conditions;
-    EquationData::PressureIC<dim>             p_initial_conditions;
-    std::map<types::global_dof_index, double> boundary_values;
-    std::vector<types::boundary_id>           boundary_ids;
+  EquationData::VelocityInflowBoundaryCondition<dim>  
+                                      inflow_boundary_condition;
+  EquationData::VelocityInitialCondition<dim>         
+                                      velocity_initial_conditions;
+  EquationData::PressureInitialCondition<dim>         
+                                      pressure_initial_conditions;
+  
+  std::map<types::global_dof_index, double> 
+                                      boundary_values;
+  std::vector<types::boundary_id>     boundary_ids;
 
-    Triangulation<dim>                        triangulation;
-    FESystem<dim>                             v_fe;
-    FE_Q<dim>                                 p_fe;
-    DoFHandler<dim>                           v_dof_handler;
-    DoFHandler<dim>                           p_dof_handler;
-    AffineConstraints<double>                 v_constraints;
-    AffineConstraints<double>                 p_constraints;
-    QGauss<dim>                               p_quadrature_formula;
-    QGauss<dim>                               v_quadrature_formula;
+  Triangulation<dim>                  triangulation;
 
-    SparsityPattern                           v_sparsity_pattern;
-    SparsityPattern                           p_sparsity_pattern;
-    SparsityPattern                           mixed_sparsity_pattern;
-    SparseMatrix<double>                      v_system_matrix;
-    SparseMatrix<double>                      v_mass_matrix;
-    SparseMatrix<double>                      v_laplace_matrix;
-    SparseMatrix<double>                      v_mass_plus_laplace_matrix;
-    SparseMatrix<double>                      v_advection_matrix;
-    SparseMatrix<double>                      p_system_matrix;
-    SparseMatrix<double>                      p_mass_matrix;
-    SparseMatrix<double>                      p_laplace_matrix;
-    SparseMatrix<double>                      p_gradient_matrix;
+  const unsigned int                  pressure_fe_degree;
+  FE_Q<dim>                           pressure_fe;
+  DoFHandler<dim>                     pressure_dof_handler;
+  AffineConstraints<double>           pressure_constraints;
+  QGauss<dim>                         pressure_quadrature_formula;
 
-    Vector<double>                            v_n;
-    Vector<double>                            v_n_m1;
-    Vector<double>                            v_extrapolated;
-    Vector<double>                            v_tmp;
-    Vector<double>                            v_rot;
-    Vector<double>                            v_rhs;
-    Vector<double>                            p_n;
-    Vector<double>                            p_n_m1;
-    Vector<double>                            p_tmp;
-    Vector<double>                            p_rhs;
-    Vector<double>                            phi_n;
-    Vector<double>                            phi_n_m1;
+  SparsityPattern                     pressure_sparsity_pattern;
+  SparsityPattern                     mixed_sparsity_pattern;
+  SparseMatrix<double>                pressure_mass_matrix;
+  SparseMatrix<double>                pressure_laplace_matrix;
+  SparseMatrix<double>                pressure_gradient_matrix;
+  //SparseMatrix<double>                pressure_system_matrix;
 
-    SparseILU<double>                         v_preconditioner;
-    SparseILU<double>                         p_preconditioner;
-    SparseDirectUMFPACK                       p_update_preconditioner;
-    SparseDirectUMFPACK                       v_rot_preconditioner;
+  Vector<double>                      pressure_n;
+  Vector<double>                      pressure_n_minus_1;
+  Vector<double>                      pressure_tmp;
+  Vector<double>                      pressure_rhs;
+  Vector<double>                      phi_n;
+  Vector<double>                      phi_n_minus_1;
 
-    DeclException2(ExcInvalidTimeStep,
-                   double,
-                   double,
-                   << " The time step " << arg1 << " is out of range."
-                   << std::endl
-                   << " The permitted range is (0," << arg2 << "]");
+  const unsigned int                  velocity_fe_degree;
+  FESystem<dim>                       velocity_fe;
+  DoFHandler<dim>                     velocity_dof_handler;
+  AffineConstraints<double>           velocity_constraints;
+  QGauss<dim>                         velocity_quadrature_formula;
 
-    void make_grid(const unsigned int n_global_refinements);
-    void setup_dofs();
-    void initialize();
-    void diffusion_step_assembly();
-    void diffusion_step_solve(const bool reinit_prec);
-    void projection_step_assembly(const bool reinit_prec);
-    void projection_step_solve(const bool reinit_prec);
-    void correction_step(const bool reinit_prec);
-    void update_time_step();
-    void output_results(const unsigned int step);
+  SparsityPattern                     velocity_sparsity_pattern;
+  SparseMatrix<double>                velocity_system_matrix;
+  SparseMatrix<double>                velocity_mass_matrix;
+  SparseMatrix<double>                velocity_laplace_matrix;
+  SparseMatrix<double>                velocity_mass_plus_laplace_matrix;
+  SparseMatrix<double>                velocity_advection_matrix;
 
-  private:
-    using IteratorTuple =
-      std::tuple<typename DoFHandler<dim>::active_cell_iterator,
-                 typename DoFHandler<dim>::active_cell_iterator>;
-    using IteratorPair = SynchronousIterators<IteratorTuple>;
+  Vector<double>                      velocity_n;
+  Vector<double>                      velocity_n_minus_1;
+  Vector<double>                      extrapolated_velocity;
+  Vector<double>                      velocity_tmp;
+  Vector<double>                      velocity_rhs;
 
-    unsigned int                              solver_max_iterations;
-    unsigned int                              solver_krylov_size;
-    unsigned int                              solver_off_diagonals;
-    unsigned int                              solver_update_preconditioner;
-    double                                    solver_tolerance;
-    double                                    solver_diag_strength;
-    bool                                      flag_adpative_time_step;
+  SparseILU<double>                   diffusion_step_preconditioner;
+  SparseILU<double>                   projection_step_preconditioner;
+  SparseDirectUMFPACK                 pressure_correction_preconditioner;
 
-    void setup_v_matrices();
-    void setup_p_matrices();
-    void setup_p_gradient_matrix();
-    void assemble_v_matrices();
-    void assemble_p_matrices();
-    void assemble_p_gradient_matrix();
-    void local_assemble_p_gradient_matrix(
-      const IteratorPair                                    &SI,
-      PressureGradientTermAssembly::LocalCellData<dim>      &scratch,
-      PressureGradientTermAssembly::MappingData<dim>        &data);
-    void mapping_p_gradient_matrix(
-      const PressureGradientTermAssembly::MappingData<dim>  &data);
+  DeclException2(ExcInvalidTimeStep,
+                 double,
+                 double,
+                 << " The time step " << arg1 << " is out of range."
+                 << std::endl
+                 << " The permitted range is (0," << arg2 << "]");
 
-    void assemble_v_advection_matrix();
-    void local_assemble_v_advection_matrix(
-      const typename DoFHandler<dim>::active_cell_iterator  &cell,
-      AdvectionTermAssembly::LocalCellData<dim>             &scratch,
-      AdvectionTermAssembly::MappingData<dim>               &data);
-    void mapping_v_advection_matrix(
-      const AdvectionTermAssembly::MappingData<dim>         &data);
-    
-    double compute_max_velocity();
-  };
+  using IteratorTuple =
+    std::tuple<typename DoFHandler<dim>::active_cell_iterator,
+               typename DoFHandler<dim>::active_cell_iterator>;
+  using IteratorPair = SynchronousIterators<IteratorTuple>;
+
+  unsigned int                        solver_max_iterations;
+  unsigned int                        solver_krylov_size;
+  unsigned int                        solver_off_diagonals;
+  unsigned int                        solver_update_preconditioner;
+  double                              solver_tolerance;
+  double                              solver_diag_strength;
+  bool                                flag_adpative_time_step;
+
+  void make_grid(const unsigned int n_global_refinements);
+  void setup_dofs();
+  void setup_matrices_and_vectors();
+  void assemble_constant_matrices();
+  void initialize();
+  void diffusion_step(const bool reinit_prec);
+  void assemble_diffusion_step();
+  void solve_diffusion_step(const bool reinit_prec);
+  void projection_step(const bool reinit_prec);
+  void assemble_projection_step();
+  void solve_projection_step(const bool reinit_prec);
+  void pressure_correction(const bool reinit_prec);
+  void update_time_step();
+  void output_results(const unsigned int step);
+
+  void setup_velocity_matrices();
+  void setup_pressure_matrices();
+  void setup_pressure_gradient_matrix();
+  void assemble_velocity_matrices();
+  void assemble_pressure_matrices();
+  void assemble_pressure_gradient_matrix();
+  void assemble_local_pressure_gradient_matrix(
+    const IteratorPair                                    &SI,
+    PressureGradientAssembly::LocalCellData<dim>          &scratch,
+    PressureGradientAssembly::MappingData<dim>            &data);
+  void copy_loca_to_global_pressure_gradient_matrix(
+    const PressureGradientAssembly::MappingData<dim>      &data);
+
+  void assemble_velocity_advection_matrix();
+  void assemble_local_velocity_advection_matrix(
+    const typename DoFHandler<dim>::active_cell_iterator  &cell,
+    AdvectionAssembly::LocalCellData<dim>                 &scratch,
+    AdvectionAssembly::MappingData<dim>                   &data);
+  void copy_local_to_global_velocity_advection_matrix(
+    const AdvectionAssembly::MappingData<dim>             &data);
+  
+  double compute_max_velocity();
+};
 
 } // namespace Step35
 
--- source/projection_solver.cc
+++ source/projection_solver.cc
@@ -16,126 +16,126 @@
 namespace Step35
 {
 
-  template <int dim>
-  NavierStokesProjection<dim>::
-  NavierStokesProjection(const RunTimeParameters::ParameterSet &data)
-    : projection_method(data.projection_method),
-      p_fe_degree(data.p_fe_degree),
-      v_fe_degree(p_fe_degree + 1),
-      dt_n(data.dt),
-      dt_n_m1(data.dt),
-      t_0(data.t_0),
-      T(data.T),
-      Re(data.Re),
-      inflow_bc(data.t_0),
-      v_fe(FE_Q<dim>(v_fe_degree), dim),
-      p_fe(p_fe_degree),
-      v_dof_handler(triangulation),
-      p_dof_handler(triangulation),
-      p_quadrature_formula(p_fe_degree + 1),
-      v_quadrature_formula(v_fe_degree + 1),
-      solver_max_iterations(data.solver_max_iterations),
-      solver_krylov_size(data.solver_krylov_size),
-      solver_off_diagonals(data.solver_off_diagonals),
-      solver_update_preconditioner(data.solver_update_preconditioner),
-      solver_tolerance(data.solver_tolerance),
-      solver_diag_strength(data.solver_diag_strength),
-      flag_adpative_time_step(data.flag_adaptive_time_step)
+template <int dim>
+NavierStokesProjection<dim>::
+NavierStokesProjection(const RunTimeParameters::ParameterSet &data)
+  : projection_method(data.projection_method),
+    dt_n(data.dt),
+    dt_n_minus_1(data.dt),
+    t_0(data.t_0),
+    T(data.T),
+    Re(data.Re),
+    inflow_boundary_condition(data.t_0),
+    pressure_fe_degree(data.p_fe_degree),
+    pressure_fe(pressure_fe_degree),
+    pressure_dof_handler(triangulation),
+    pressure_quadrature_formula(pressure_fe_degree + 1),
+    velocity_fe_degree(pressure_fe_degree + 1),
+    velocity_fe(FE_Q<dim>(velocity_fe_degree), dim),
+    velocity_dof_handler(triangulation),
+    velocity_quadrature_formula(velocity_fe_degree + 1),
+    solver_max_iterations(data.solver_max_iterations),
+    solver_krylov_size(data.solver_krylov_size),
+    solver_off_diagonals(data.solver_off_diagonals),
+    solver_update_preconditioner(data.solver_update_preconditioner),
+    solver_tolerance(data.solver_tolerance),
+    solver_diag_strength(data.solver_diag_strength),
+    flag_adpative_time_step(data.flag_adaptive_time_step)
+{
+  if (pressure_fe_degree < 1)
+    std::cout
+      << " WARNING: The chosen pair of finite element spaces is not stable."
+      << std::endl
+      << " The obtained results will be nonsense" << std::endl;
+
+  AssertThrow(!((dt_n <= 0.) || (dt_n > .5 * T)), 
+              ExcInvalidTimeStep(dt_n, .5 * T));
+
+  make_grid(data.n_global_refinements);
+  setup_dofs();
+  setup_matrices_and_vectors();
+  assemble_constant_matrices();
+  initialize();
+}
+
+template <int dim>
+void NavierStokesProjection<dim>::
+run(const bool  flag_verbose_output,
+    const unsigned int output_interval)
+{
+  ConditionalOStream verbose_cout(std::cout, flag_verbose_output);
+
+  //const auto n_steps = static_cast<unsigned int>((T - t_0) / dt);
+
+  inflow_boundary_condition.set_time(2. * dt_n);
+  output_results(1);
+  unsigned int n = 2;
+  
+  for (DiscreteTime time(2 * dt_n, T, dt_n);
+        time.get_current_time() <= time.get_end_time();
+        time.advance_time())
   {
-    if (p_fe_degree < 1)
-      std::cout
-        << " WARNING: The chosen pair of finite element spaces is not stable."
-        << std::endl
-        << " The obtained results will be nonsense" << std::endl;
+    verbose_cout << "  Diffusion Step" << std::endl;
+    if (n % solver_update_preconditioner == 0)
+      verbose_cout << "    With reinitialization of the preconditioner"
+                    << std::endl;
+    diffusion_step((n % solver_update_preconditioner == 0) || (n == 2));
 
-    AssertThrow(!((dt_n <= 0.) || (dt_n > .5 * T)), 
-                ExcInvalidTimeStep(dt_n, .5 * T));
-
-    make_grid(data.n_global_refinements);
-    setup_dofs();
-    initialize();
-  }
-
-  template <int dim>
-  void NavierStokesProjection<dim>::
-  run(const bool  flag_verbose_output,
-      const unsigned int output_interval)
-  {
-    ConditionalOStream verbose_cout(std::cout, flag_verbose_output);
-
-    //const auto n_steps = static_cast<unsigned int>((T - t_0) / dt);
-
-    inflow_bc.set_time(2. * dt_n);
-    output_results(1);
-    unsigned int n = 2;
+    verbose_cout << "  Projection Step" << std::endl;
+    projection_step((n == 2));
     
-    for (DiscreteTime time(2 * dt_n, T, dt_n);
-         time.get_current_time() <= time.get_end_time();
-         time.advance_time())
-    {
-      verbose_cout << "  Diffusion Step" << std::endl;
-      if (n % solver_update_preconditioner == 0)
-        verbose_cout << "    With reinitialization of the preconditioner"
-                      << std::endl;
-      diffusion_step_assembly();
-      diffusion_step_solve((n % solver_update_preconditioner == 0) || (n == 2));
+    verbose_cout << "  Updating the Pressure" << std::endl;
+    pressure_correction((n == 2));
 
-      verbose_cout << "  Projection Step" << std::endl;
-      projection_step_assembly((n == 2));
-      projection_step_solve((n==2));
-      
-      verbose_cout << "  Updating the Pressure" << std::endl;
-      correction_step((n == 2));
+    if ((n % output_interval == 0) || time.is_at_end())
+      {
+        verbose_cout << "Plotting Solution" << std::endl;
+        output_results(n);
+      }
 
-      if ((n % output_interval == 0) || time.is_at_end())
-        {
-          verbose_cout << "Plotting Solution" << std::endl;
-          output_results(n);
-        }
+    if ((flag_adpative_time_step) && (n > 20))
+      update_time_step();
 
-      if ((flag_adpative_time_step) && (n > 20))
-        update_time_step();
+    Point<dim> evaluation_point;
+    evaluation_point(0) = 2.0;
+    evaluation_point(1) = 3.0;
 
-      Point<dim> evaluation_point;
-      evaluation_point(0) = 2.0;
-      evaluation_point(1) = 3.0;
+    Vector<double> point_value_velocity(dim);
+    VectorTools::point_value(velocity_dof_handler,
+                            velocity_n,
+                            evaluation_point,
+                            point_value_velocity);
 
-      Vector<double> point_value_velocity(dim);
-      VectorTools::point_value(v_dof_handler,
-                              v_n,
-                              evaluation_point,
-                              point_value_velocity);
+    const double point_value_pressure
+    = VectorTools::point_value(pressure_dof_handler,
+                              pressure_n,
+                              evaluation_point);
+    std::cout << "Step = " 
+              << std::setw(2) 
+              << n 
+              << " Time = " 
+              << std::noshowpos << std::scientific
+              << time.get_current_time()
+              << " Velocity = (" 
+              << std::showpos << std::scientific
+              << point_value_velocity[0] 
+              << ", "
+              << std::showpos << std::scientific
+              << point_value_velocity[1] 
+              << ") Pressure = "
+              << std::showpos << std::scientific
+              << point_value_pressure 
+              << " Time step = " 
+              << std::showpos << std::scientific
+              << dt_n << std::endl;
 
-      const double point_value_pressure
-      = VectorTools::point_value(p_dof_handler,
-                                p_n,
-                                evaluation_point);
-      std::cout << "Step = " 
-                << std::setw(2) 
-                << n 
-                << " Time = " 
-                << std::noshowpos << std::scientific
-                << time.get_current_time()
-                << " Velocity = (" 
-                << std::showpos << std::scientific
-                << point_value_velocity[0] 
-                << ", "
-                << std::showpos << std::scientific
-                << point_value_velocity[1] 
-                << ") Pressure = "
-                << std::showpos << std::scientific
-                << point_value_pressure 
-                << " Time step = " 
-                << std::showpos << std::scientific
-                << dt_n << std::endl;
-
-      time.set_desired_next_step_size(dt_n);
-      inflow_bc.advance_time(dt_n);
-      ++n;
-      if (time.is_at_end())
-        break;
-    }
+    time.set_desired_next_step_size(dt_n);
+    inflow_boundary_condition.advance_time(dt_n);
+    ++n;
+    if (time.is_at_end())
+      break;
   }
+}
 }  // namespace Step35
 
 // explicit instantiations
