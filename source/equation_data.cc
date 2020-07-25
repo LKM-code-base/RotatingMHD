f0553334a6958c616f0f4f26a7c982eb6c396a84
Jose <jose.lara-hernandez@bam.de>
2020-07-25 15:20:26 +0200

    Identation changes

--- source/equation_data.cc
+++ source/equation_data.cc
@@ -11,65 +11,68 @@ namespace Step35
 {
   using namespace dealii;
 
-  namespace EquationData
-  {
+namespace EquationData
+{
 
-    template <int dim>
-    VelocityIC<dim>::VelocityIC(const double time)
-      : Function<dim>(dim, time)
-    {}
+template <int dim>
+VelocityInitialCondition<dim>::VelocityInitialCondition(const double time)
+  : Function<dim>(dim, time)
+{}
 
-    template <int dim>
-    void VelocityIC<dim>::vector_value(const Point<dim>  &p,
-                                      Vector<double>    &values) const
-    {
-        (void)p;
-        values[0] = 0.0;
-        values[1] = 0.0;
-    }
-
-    template <int dim>
-    VelocityInflowBC<dim>::VelocityInflowBC(const double time)
-    : Function<dim>(dim, time)
-    {}
-
-    template <int dim>
-    void VelocityInflowBC<dim>::vector_value(
+template <int dim>
+void VelocityInitialCondition<dim>::vector_value(
                                         const Point<dim>  &p,
                                         Vector<double>    &values) const
-    {
-      const double Um = 1.5;
-      const double H  = 4.1;
+{
+    (void)p;
+    values[0] = 0.0;
+    values[1] = 0.0;
+}
 
-      values[0] = 4.0 * Um * p(1) * ( H - p(1) ) / ( H * H );
-      values[1] = 0.0;
-    }
+template <int dim>
+VelocityInflowBoundaryCondition<dim>::VelocityInflowBoundaryCondition(
+                                                      const double time)
+: Function<dim>(dim, time)
+{}
 
-    template <int dim>
-    PressureIC<dim>::PressureIC(const double time)
-    : Function<dim>(1, time)
-    {}
+template <int dim>
+void VelocityInflowBoundaryCondition<dim>::vector_value(
+                                        const Point<dim>  &p,
+                                        Vector<double>    &values) const
+{
+  const double Um = 1.5;
+  const double H  = 4.1;
 
-    template<int dim>
-    double PressureIC<dim>::value(const Point<dim> &p,
-                                  const unsigned int component) const
-    {
-      (void)component;
-      return 25.0 - p(0);
-    }
+  values[0] = 4.0 * Um * p(1) * ( H - p(1) ) / ( H * H );
+  values[1] = 0.0;
+}
 
-  } // namespace EquationData
+template <int dim>
+PressureInitialCondition<dim>::PressureInitialCondition(const double time)
+: Function<dim>(1, time)
+{}
+
+template<int dim>
+double PressureInitialCondition<dim>::value(
+                                    const Point<dim> &p,
+                                    const unsigned int component) const
+{
+  (void)component;
+  return 25.0 - p(0);
+}
+
+} // namespace EquationData
 
 } // namespace Step35
 
 
 // explicit instantiation
 
-template class Step35::EquationData::VelocityIC<2>;
-template class Step35::EquationData::VelocityIC<3>;
+template class Step35::EquationData::VelocityInitialCondition<2>;
+template class Step35::EquationData::VelocityInitialCondition<3>;
 
-template class Step35::EquationData::VelocityInflowBC<2>;
-template class Step35::EquationData::VelocityInflowBC<3>;
+template class Step35::EquationData::VelocityInflowBoundaryCondition<2>;
+template class Step35::EquationData::VelocityInflowBoundaryCondition<3>;
 
-template class Step35::EquationData::PressureIC<2>;
-template class Step35::EquationData::PressureIC<3>;
+template class Step35::EquationData::PressureInitialCondition<2>;
+template class Step35::EquationData::PressureInitialCondition<3>;
