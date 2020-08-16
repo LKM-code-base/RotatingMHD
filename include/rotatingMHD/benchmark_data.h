458a3800fb62746933f4eb3d062c6a8028eae8a1
Jose <jose.lara-hernandez@bam.de>
2020-08-16 14:13:38 +0200

    Added methods to the DFG struct to calculate most of the parameters, as well as to gather them in a table and terminal output.

--- include/rotatingMHD/benchmark_data.h
+++ include/rotatingMHD/benchmark_data.h
@@ -5,7 +5,13 @@
 #include <deal.II/base/discrete_time.h>
 #include <deal.II/base/point.h>
 #include <deal.II/base/table_handler.h>
+#include <deal.II/base/utilities.h>
+#include <deal.II/distributed/tria.h>
 #include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/lac/trilinos_vector.h>
 
 #include <iostream>
 #include <vector>
@@ -24,6 +30,8 @@ struct DFG
   Point<dim>    front_evaluation_point;
   Point<dim>    rear_evaluation_point;
   double        pressure_difference;
+  double        front_point_pressure_value;
+  double        rear_point_pressure_value;
   double        drag_force;
   double        drag_coefficient;
   double        max_drag_coefficient;
@@ -39,13 +47,29 @@ struct DFG
   double        frequency;
   double        strouhal_number;
   TableHandler  data_table;
+  bool          flag_min_reached;
 
   DFG();
   void compute_pressure_difference(
               const DoFHandler<dim>               &pressure_dof_handler,
               const TrilinosWrappers::MPI::Vector &pressure_n);
+  void compute_drag_and_lift_forces_and_coefficients(
+  const parallel::distributed::Triangulation<dim> &triangulation,
+  const FESystem<dim>                             &velocity_fe,
+  const unsigned int                              &velocity_fe_degree,
+  const DoFHandler<dim>                           &velocity_dof_handler,
+  const TrilinosWrappers::MPI::Vector             &velocity_n,
+  const FE_Q<dim>                                 &pressure_fe,
+  const DoFHandler<dim>                           &pressure_dof_handler,
+  const TrilinosWrappers::MPI::Vector             &pressure_n,
+  const double                                    &Re);
   void compute_periodic_parameters();
-  void update_table();
+  void update_table(const unsigned int    &step,
+                    DiscreteTime          &time);
+  void print_step_data(const unsigned int &step,
+                       DiscreteTime       &time);
+  void print_table();
+  void write_table_to_file(const std::string &file);
 };
 
 } // namespace BenchmarkData
