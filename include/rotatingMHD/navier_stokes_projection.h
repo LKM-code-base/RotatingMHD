b4d5f51970ecbdaf7853c469df9f6a9e55f3f89a
sebglane <seb_glane@gmx.de>
2020-08-21 18:13:35 +0200

    Added a first rather simple documentation of the projection solver

--- include/rotatingMHD/navier_stokes_projection.h
+++ include/rotatingMHD/navier_stokes_projection.h
@@ -16,11 +16,22 @@
 namespace RMHD
 {
 
-  using namespace dealii;
+using namespace dealii;
 
+
+/*!
+ * @brief Solves the Navier Stokes equations with the incremental pressure
+ * projection scheme.
+ *
+ * @details This version is parallelized using deal.ii's MPI facilities and
+ * relies either on the Trilinos or the PETSc library. Moreover, for the time
+ * discretization an implicit-explicit scheme (IMEX) with variable step size is
+ * used.
+ */
 template <int dim>
 class NavierStokesProjection
 {
+
 public:
   NavierStokesProjection(
                 const RunTimeParameters::ParameterSet   &parameters,
@@ -28,42 +39,164 @@ public:
                 Entities::Pressure<dim>                 &pressure,
                 TimeDiscretization::VSIMEXCoefficients  &VSIMEX,
                 TimeDiscretization::VSIMEXMethod        &time_stepping);
+
   void setup();
+
+
+  /*!
+   *  @brief Solves the problem for one single timestep.
+   *
+   *  @details Performs the diffusion and the projection step for one single
+   *  time step and updates the member variables at the end.
+   */
+
   void solve(const unsigned int step);
   
 private:
+
+
+  /*!
+   *  @brief A parameter which determines the type of the pressure update.
+   *
+   *  @details For the pressure update after the projection step, this parameter
+   *  determines whether the irrotational form of the pressure update is used or
+   *  not.
+   */
   RunTimeParameters::ProjectionMethod     projection_method;
+
+
+  /*!
+   * @brief The Reynolds number.
+   *
+   *  @details A parameter which determines the ratio of convection to viscous
+   *  diffusion.
+   */
   const double                            Re;
+
+  /*!
+   * @brief Size of the current timestep.
+   */
   double                                  dt_n;
+
+  /*!
+   * @brief Size of the previous timestep.
+   */
   double                                  dt_n_minus_1;
+
+  /*!
+   * @brief A reference to the entity of velocity field.
+   */
   Entities::Velocity<dim>                 &velocity;
+
+  /*!
+   * @brief A reference to the entity of the pressure field.
+   */
   Entities::Pressure<dim>                 &pressure;
 
   TimeDiscretization::VSIMEXCoefficients  &VSIMEX;
+
   TimeDiscretization::VSIMEXMethod        &time_stepping;
 
+  /*!
+   * @brief System matrix used to solve for the velocity field in the diffusion
+   * step.
+   *
+   * @details This matrix changes in every timestep because the convective term
+   * needs to be assembled in every timestep. However, not the entire system
+   * matrix is assembled in every timestep but only the part due to the
+   * convective term.
+   */
   TrilinosWrappers::SparseMatrix        velocity_system_matrix;
+
+  /*!
+   * @brief Mass matrix of the velocity.
+   *
+   * @details This matrix does change not in every timestep. It is stored in
+   * memory because otherwise an assembly would be required if the timestep
+   * changes.
+   */
   TrilinosWrappers::SparseMatrix        velocity_mass_matrix;
+
+  /*!
+   * @brief Stiffness matrix of the velocity. Assembly of  the weak of the
+   * Laplace operator.
+   *
+   * @details This matrix does not change in every timestep. It is stored in
+   * memory because otherwise an assembly would be required if the timestep
+   * changes.
+   */
   TrilinosWrappers::SparseMatrix        velocity_laplace_matrix;
+
+  /*!
+   * @brief Sum of the mass and the stiffness matrix of the velocity.
+   *
+   * @details This matrix does not change in every timestep. It is stored in
+   * memory because otherwise an assembly would be required if the timestep
+   * changes.
+   */
   TrilinosWrappers::SparseMatrix        velocity_mass_plus_laplace_matrix;
+
+  /*!
+   * @brief Matrix representing the assembly of the skew-symmetric formm of the
+   * convective term.
+   *
+   * @details This matrix changes in every timestep and is therefore also
+   * assembled in every timestep.
+   */
   TrilinosWrappers::SparseMatrix        velocity_advection_matrix;
 
+
+  /*!
+   * @brief Auxiliary vector which represents the velocity field which is
+   * extrapolated to the time of the considered timestep.
+   */
   TrilinosWrappers::MPI::Vector         extrapolated_velocity;
+
+  // What is the purpose of this object?
   TrilinosWrappers::MPI::Vector         velocity_tmp;
+
+  /*!
+   * @brief Vector representing the right-hand side of the linear system of the
+   * diffusion step.
+   */
   TrilinosWrappers::MPI::Vector         velocity_rhs;
 
+  /*!
+   * @brief Mass matrix of the pressure field.
+   */
   TrilinosWrappers::SparseMatrix        pressure_mass_matrix;
+
+  /*!
+   * @brief Stiffness matrix of the pressure field. Assembly of  the weak of the
+   * Laplace operator.
+   */
   TrilinosWrappers::SparseMatrix        pressure_laplace_matrix;
 
+  // What is the purpose of this object?
   TrilinosWrappers::MPI::Vector         pressure_tmp;
+
+  /*!
+   * @brief Vector representing the right-hand side of the linear system of the
+   * projection step.
+   */
   TrilinosWrappers::MPI::Vector         pressure_rhs;
+
+  /*!
+   * @brief Vector representing the pressure update of the current timestep.
+   */
   TrilinosWrappers::MPI::Vector         phi_n;
+
+  /*!
+   * @brief Vector representing the pressure update of the previous timestep.
+   */
   TrilinosWrappers::MPI::Vector         phi_n_minus_1;
+
 
   TrilinosWrappers::PreconditionILU     diffusion_step_preconditioner;
   TrilinosWrappers::PreconditionILU     projection_step_preconditioner;
   TrilinosWrappers::PreconditionJacobi  correction_step_preconditioner;
 
+  // SG thinks that all of these parameters can go into a parameter structure.
   unsigned int                          solver_max_iterations;
   unsigned int                          solver_krylov_size;
   unsigned int                          solver_off_diagonals;
@@ -72,56 +205,174 @@ private:
   double                                solver_diag_strength;
   bool                                  flag_adpative_time_step;
 
+
+  /*!
+   * @brief Setup of the sparsity spatterns of the matrices of the diffusion and
+   * projection steps.
+   */
   void setup_matrices();
+
+  /*!
+   * @brief Setup of the right-hand side and the auxiliary vector of the
+   * diffusion and projection step.
+   */
   void setup_vectors();
+
+  /*!
+   * @brief Assemble the matrices which change only if the triangulation is
+   * refined or coarsened.
+   */
   void assemble_constant_matrices();
+
+  /*!
+   * @brief Currently this method only sets the vector of the two pressure
+   * updates @ref phi_n and @ref phi_n_minus_1 to zero.
+   */
   void initialize();
 
+  /*!
+   * @brief This method performs one complete diffusion step.
+   */
   void diffusion_step(const bool reinit_prec);
+
+  /*!
+   * @brief This method assembles the system matrix and the right-hand side of
+   * the diffusion step.
+   */
   void assemble_diffusion_step();
+
+  /*!
+   * @brief This method solves the linear system of the diffusion step.
+   */
   void solve_diffusion_step(const bool reinit_prec);
+
+  /*!
+   * @brief This method performs one complete projection step.
+   */
   void projection_step(const bool reinit_prec);
+
+  /*!
+   * @brief This method assembles the linear system of the projection step.
+   */
   void assemble_projection_step();
+
+  /*!
+   * @brief This method solves the linear system of the projection step.
+   */
   void solve_projection_step(const bool reinit_prec);
+
+  /*!
+   * @brief This method performs the pressure update of the projection step.
+   */
   void pressure_correction(const bool reinit_prec);
 
+  /*!
+   * @brief This method assembles the mass and the stiffness matrix of the
+   * velocity field using the WorkStream approach.
+   */
   void assemble_velocity_matrices();
+
+  /*!
+   * @brief This method assembles the local mass and the local stiffness
+   * matrices of the velocity field on a single cell.
+   */
   void assemble_local_velocity_matrices(
     const typename DoFHandler<dim>::active_cell_iterator  &cell,
     VelocityMatricesAssembly::LocalCellData<dim>          &scratch,
     VelocityMatricesAssembly::MappingData<dim>            &data);
+
+  /*!
+   * @brief This method copies the local mass and the local stiffness matrices
+   * of the velocity field on a single cell into the global matrices.
+   */
   void copy_local_to_global_velocity_matrices(
     const VelocityMatricesAssembly::MappingData<dim>      &data);
 
+  /*!
+   * @brief This method copies the local mass and the local stiffness matrices
+   * of the velocity field into the global matrices.
+   */
   void assemble_pressure_matrices();
+
+  /*!
+   * @brief This method assembles the local mass and the local stiffness
+   * matrices of the velocity field on a single cell.
+   */
   void assemble_local_pressure_matrices(
     const typename DoFHandler<dim>::active_cell_iterator  &cell,
     PressureMatricesAssembly::LocalCellData<dim>          &scratch,
     PressureMatricesAssembly::MappingData<dim>            &data);
+
+  /*!
+   * @brief This method copies the local mass and the local stiffness matrices
+   * of the pressure field on a single cell into the global matrices.
+   */
   void copy_local_to_global_pressure_matrices(
     const PressureMatricesAssembly::MappingData<dim>      &data);
 
+  /*!
+   * @brief This method assembles the right-hand side of the diffusion step
+   * using the WorkStream approach.
+   */
   void assemble_diffusion_step_rhs();
+
+  /*!
+   * @brief This method assembles the local right-hand side of the diffusion
+   * step on a single cell.
+   */
   void assemble_local_diffusion_step_rhs(
     const typename DoFHandler<dim>::active_cell_iterator  &cell,
     VelocityRightHandSideAssembly::LocalCellData<dim>     &scratch,
     VelocityRightHandSideAssembly::MappingData<dim>       &data);
+
+  /*!
+   * @brief This method copies the local right-hand side of the diffusion step
+   * into the global vector.
+   */
   void copy_local_to_global_diffusion_step_rhs(
     const VelocityRightHandSideAssembly::MappingData<dim> &data);
 
+  /*!
+   * @brief This method assembles the right-hand side of the projection step
+   * using the WorkStream approach.
+   */
   void assemble_projection_step_rhs();
+
+  /*!
+   * @brief This method assembles the local right-hand side of the projection
+   * step on a single cell.
+   */
   void assemble_local_projection_step_rhs(
     const typename DoFHandler<dim>::active_cell_iterator  &cell,
     PressureRightHandSideAssembly::LocalCellData<dim>     &scratch,
     PressureRightHandSideAssembly::MappingData<dim>       &data);
+
+  /*!
+   * @brief This method copies the local right-hand side of the projection step
+   * on a single cell to the global vector.
+   */
   void copy_local_to_global_projection_step_rhs(
     const PressureRightHandSideAssembly::MappingData<dim> &data);
 
+  /*!
+   * @brief This method assembles the velocity advection matrix using the
+   * WorkStream approach.
+   */
   void assemble_velocity_advection_matrix();
+
+  /*!
+   * @brief This method assembles the local velocity advection matrix on a
+   * single cell.
+   */
   void assemble_local_velocity_advection_matrix(
     const typename DoFHandler<dim>::active_cell_iterator  &cell,
     AdvectionAssembly::LocalCellData<dim>                 &scratch,
     AdvectionAssembly::MappingData<dim>                   &data);
+
+  /*!
+   * @brief This method copies the local velocity advection matrix into the
+   * global matrix.
+   */
   void copy_local_to_global_velocity_advection_matrix(
     const AdvectionAssembly::MappingData<dim>             &data);
 };
