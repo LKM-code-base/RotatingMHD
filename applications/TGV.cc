6ab1c31e6fc25fc96adf075d7f7e6fdb7ab7ef22
Jose <jose.lara-hernandez@bam.de>
2020-10-29 00:27:39 +0100

    Moved convergence struct to its own .h and .cc files. Added convergence .txt output to the source fiels.

--- applications/TGV.cc
+++ applications/TGV.cc
@@ -3,6 +3,7 @@
  *@brief The .cc file solving the TGV benchmark.
  */
 #include <rotatingMHD/benchmark_data.h>
+#include <rotatingMHD/convergence_struct.h>
 #include <rotatingMHD/entities_structs.h>
 #include <rotatingMHD/equation_data.h>
 #include <rotatingMHD/navier_stokes_projection.h>
@@ -10,156 +11,16 @@
 #include <rotatingMHD/run_time_parameters.h>
 #include <rotatingMHD/time_discretization.h>
 
-#include <deal.II/base/conditional_ostream.h>
-#include <deal.II/base/convergence_table.h>
-#include <deal.II/base/utilities.h>
-#include <deal.II/dofs/dof_tools.h>
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_in.h>
 #include <deal.II/grid/grid_tools.h>
-#include <deal.II/grid/manifold_lib.h>
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cmath>
+
+#include <memory>
 
 namespace RMHD
 {
   using namespace dealii;
-
-template <int dim>
-struct ConvergenceAnalysisData
-{
-  ConvergenceTable                convergence_table;
-
-  const std::string               entity_name;
-
-  const Entities::EntityBase<dim> &entity;
-
-  const Function<dim>             &exact_solution;
-
-  ConvergenceAnalysisData(const Entities::EntityBase<dim> &entity,
-                          const Function<dim>             &exact_solution,
-                          const std::string entity_name = "Entity")
-  :
-  entity_name(entity_name),
-  entity(entity),
-  exact_solution(exact_solution)
-  {
-    convergence_table.declare_column("level");
-    convergence_table.declare_column("dt");
-    convergence_table.declare_column("cells");
-    convergence_table.declare_column("dofs");
-    convergence_table.declare_column("hmax");
-    convergence_table.declare_column("L2");
-    convergence_table.declare_column("H1");
-    convergence_table.declare_column("Linfty");
-    convergence_table.set_scientific("dt", true);
-    convergence_table.set_scientific("hmax", true);
-    convergence_table.set_scientific("L2", true);
-    convergence_table.set_scientific("H1", true);
-    convergence_table.set_scientific("Linfty", true);
-    convergence_table.set_precision("dt", 2);
-    convergence_table.set_precision("hmax", 2);
-    convergence_table.set_precision("L2", 6);
-    convergence_table.set_precision("H1", 6);
-    convergence_table.set_precision("Linfty", 6);
-  }
-
-  void update_table(const unsigned int  &level,
-                    const double        &time_step,
-                    const bool          &flag_spatial_convergence)
-  {
-  Vector<double> cellwise_difference(
-    entity.dof_handler.get_triangulation().n_active_cells());
-
-  QGauss<dim>    quadrature_formula(entity.fe_degree + 2);
-  const QTrapez<1>     trapezoidal_rule;
-  const QIterated<dim> iterated_quadrature_rule(trapezoidal_rule,
-                                                entity.fe_degree * 2 + 1);
-  
-  VectorTools::integrate_difference(entity.dof_handler,
-                                    entity.solution,
-                                    exact_solution,
-                                    cellwise_difference,
-                                    quadrature_formula,
-                                    VectorTools::L2_norm);
-  
-  const double L2_error =
-    VectorTools::compute_global_error(entity.dof_handler.get_triangulation(),
-                                      cellwise_difference,
-                                      VectorTools::L2_norm);
-
-  VectorTools::integrate_difference(entity.dof_handler,
-                                    entity.solution,
-                                    exact_solution,
-                                    cellwise_difference,
-                                    quadrature_formula,
-                                    VectorTools::H1_norm);
-  
-  const double H1_error =
-    VectorTools::compute_global_error(entity.dof_handler.get_triangulation(),
-                                      cellwise_difference,
-                                      VectorTools::H1_norm);
-
-  VectorTools::integrate_difference(entity.dof_handler,
-                                    entity.solution,
-                                    exact_solution,
-                                    cellwise_difference,
-                                    iterated_quadrature_rule,
-                                    VectorTools::Linfty_norm);
-  
-  const double Linfty_error =
-    VectorTools::compute_global_error(entity.dof_handler.get_triangulation(),
-                                      cellwise_difference,
-                                      VectorTools::Linfty_norm);
-
-  convergence_table.add_value("level", level);
-  convergence_table.add_value("dt", time_step);
-  convergence_table.add_value("cells", entity.dof_handler.get_triangulation().n_active_cells());
-  convergence_table.add_value("dofs", entity.dof_handler.n_dofs());
-  convergence_table.add_value("hmax", GridTools::maximal_cell_diameter(entity.dof_handler.get_triangulation()));
-  convergence_table.add_value("L2", L2_error);
-  convergence_table.add_value("H1", H1_error);
-  convergence_table.add_value("Linfty", Linfty_error);
-
-  std::string reference_column = (flag_spatial_convergence) ? 
-                                  "hmax" : "dt";
-
-  convergence_table.evaluate_convergence_rates(
-                              "L2",
-                              reference_column,
-                              ConvergenceTable::reduction_rate_log2,
-                              1);
-  convergence_table.evaluate_convergence_rates(
-                              "H1",
-                              reference_column,
-                              ConvergenceTable::reduction_rate_log2,
-                              1);
-  convergence_table.evaluate_convergence_rates(
-                              "Linfty",
-                              reference_column,
-                              ConvergenceTable::reduction_rate_log2,
-                              1);
-  }
-
-  void print_table()
-  {
-    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
-    {
-    std::cout << std::endl;
-    std::cout 
-      << "                               " << entity_name 
-      << " convergence table" << std::endl
-      << "==============================================="
-      << "===============================================" 
-      << std::endl;
-    convergence_table.write_text(std::cout);
-    }
-  }
-};
 
 /*!
  * @class TGV
@@ -424,14 +285,14 @@ void TGV<dim>::postprocessing(const bool flag_point_evaluation)
   {
     std::cout.precision(1);
     *(this->pcout)  << time_stepping
-                    << " D_norm = "
+                    << " Norms = ("
                     << std::noshowpos << std::scientific
                     << navier_stokes.get_diffusion_step_rhs_norm()
-                    << " P_norm = "
+                    << ", "
                     << navier_stokes.get_projection_step_rhs_norm()
-                    << " CFL = "
+                    << ") CFL = "
                     << navier_stokes.get_cfl_number()
-                    << " Progress ["
+                    << " ["
                     << std::setw(5) 
                     << std::fixed
                     << time_stepping.get_next_time()/time_stepping.get_end_time() * 100.
@@ -621,8 +482,16 @@ void TGV<dim>::run(const bool flag_convergence_test)
     }
   }
   
-  velocity_convergence_table.print_table();
-  pressure_convergence_table.print_table();
+  std::string tablefilename = (this->prm.flag_spatial_convergence_test) ?
+                              "TGVSpatialTest" : 
+                              "TGVTemporalTest_Level" + 
+                              std::to_string(this->prm.initial_refinement_level);
+  tablefilename += "_Re" + std::to_string((int)this->prm.Re);
+
+  velocity_convergence_table.print_table_to_terminal();
+  velocity_convergence_table.print_table_to_file(tablefilename + "_Velocity");
+  pressure_convergence_table.print_table_to_terminal();
+  pressure_convergence_table.print_table_to_file(tablefilename + "_Pressure");
 }
 
 } // namespace RMHD
