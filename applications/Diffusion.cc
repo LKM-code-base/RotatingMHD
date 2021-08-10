0291d08fdd16966b8ae30419c8fb890313d6f535
sebglane <seb_glane@gmx.de>
2021-08-10 16:53:09 +0200

    Diffusion test converging...

--- applications/Diffusion.cc
+++ applications/Diffusion.cc
@@ -1,10 +1,10 @@
 #include <rotatingMHD/convection_diffusion_solver.h>
-#include <rotatingMHD/convergence_test.h>
 #include <rotatingMHD/entities_structs.h>
 #include <rotatingMHD/problem_class.h>
 #include <rotatingMHD/run_time_parameters.h>
 #include <rotatingMHD/time_discretization.h>
 
+#include <deal.II/base/convergence_table.h>
 #include <deal.II/base/function.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
@@ -98,13 +98,11 @@ private:
 
   std::shared_ptr<Entities::ScalarEntity<dim>>  scalar_field;
 
-  std::shared_ptr<Function<dim>>                exact_solution;
-
   TimeDiscretization::VSIMEXMethod              time_stepping;
 
   HeatEquation<dim>                             advection_diffusion;
 
-  ConvergenceAnalysisData<dim>                  convergence_table;
+  ConvergenceTable                              convergence_table;
 
   void make_grid(const unsigned int n_global_refinements);
 
@@ -114,9 +112,11 @@ private:
 
   void initialize();
 
-  void solve(const unsigned int level);
+  void solve();
 
   void postprocessing();
+
+  void process_solution(const unsigned int);
 
   void output();
 
@@ -135,17 +135,13 @@ scalar_field(std::make_shared<Entities::ScalarEntity<dim>>(
   parameters.fe_degree_temperature,
   this->triangulation,
   "Scalar field")),
-exact_solution(std::make_shared<ExactSolution<dim>>(
-  parameters.Pe,
-  parameters.time_discretization_parameters.start_time)),
 time_stepping(parameters.time_discretization_parameters),
 advection_diffusion(parameters.heat_equation_parameters,
                     time_stepping,
                     scalar_field,
                     this->mapping,
                     this->pcout,
-                    this->computing_timer),
-convergence_table(scalar_field, *exact_solution)
+                    this->computing_timer)
 {}
 
 
@@ -217,28 +213,65 @@ void Diffusion<dim>::initialize()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Setup - Initial conditions");
 
+  ExactSolution<dim> initial_condition(parameters.Pe);
+
   this->set_initial_conditions(scalar_field,
-                               *exact_solution,
+                               initial_condition,
                                time_stepping);
 }
 
 
 
 template <int dim>
-void Diffusion<dim>::postprocessing()
+void Diffusion<dim>::process_solution(const unsigned int cycle)
 {
-  TimerOutput::Scope  t(*this->computing_timer, "Problem: Postprocessing");
+  const double current_time{time_stepping.get_current_time()};
 
-  std::cout.precision(1);
-  *this->pcout  << time_stepping
-                << " Norm = "
-                << std::noshowpos << std::scientific
-                << advection_diffusion.get_rhs_norm()
-                << " Progress ["
-                << std::setw(5)
-                << std::fixed
-                << time_stepping.get_next_time()/time_stepping.get_end_time() * 100.
-                << "%] \r";
+  Vector<float> difference_per_cell(this->triangulation.n_active_cells());
+  VectorTools::integrate_difference(*scalar_field->dof_handler,
+                                    scalar_field->solution,
+                                    ExactSolution<dim>(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    QGauss<dim>(scalar_field->fe_degree + 1),
+                                    VectorTools::L2_norm);
+  const double L2_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::L2_norm);
+  VectorTools::integrate_difference(*scalar_field->dof_handler,
+                                    scalar_field->solution,
+                                    ExactSolution<dim>(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    QGauss<dim>(scalar_field->fe_degree + 1),
+                                    VectorTools::H1_seminorm);
+  const double H1_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::H1_seminorm);
+  const QTrapez<1>     q_trapez;
+  const QIterated<dim> q_iterated(q_trapez, scalar_field->fe_degree * 2 + 1);
+  VectorTools::integrate_difference(*scalar_field->dof_handler,
+                                    scalar_field->solution,
+                                    ExactSolution<dim>(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    q_iterated,
+                                    VectorTools::Linfty_norm);
+  const double Linfty_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::Linfty_norm);
+
+  const unsigned int n_active_cells = this->triangulation.n_global_active_cells();
+  const unsigned int n_dofs         = scalar_field->dof_handler->n_dofs();
+  convergence_table.add_value("cycle", cycle);
+  convergence_table.add_value("time_step", time_stepping.get_previous_step_size());
+  convergence_table.add_value("cells", n_active_cells);
+  convergence_table.add_value("dofs", n_dofs);
+  convergence_table.add_value("L2", L2_error);
+  convergence_table.add_value("H1", H1_error);
+  convergence_table.add_value("Linfty", Linfty_error);
+
+
 }
 
 
@@ -267,17 +300,19 @@ void Diffusion<dim>::output()
 
 
 template <int dim>
-void Diffusion<dim>::solve(const unsigned int level)
+void Diffusion<dim>::solve()
 {
   setup_dofs();
+
   setup_constraints();
+
   scalar_field->reinit();
+
   initialize();
 
   // Outputs the fields at t_0, i.e. the initial conditions.
   {
     scalar_field->solution = scalar_field->old_solution;
-    exact_solution->set_time(time_stepping.get_start_time());
     output();
   }
 
@@ -286,10 +321,8 @@ void Diffusion<dim>::solve(const unsigned int level)
     // The VSIMEXMethod instance starts each loop at t^{k-1}
 
     // Updates the coefficients to their k-th value
+    *this->pcout << time_stepping << std::endl;
     time_stepping.update_coefficients();
-
-    // Updates the functions and the constraints to t^{k}
-    exact_solution->set_time(time_stepping.get_next_time());
 
     scalar_field->boundary_conditions.set_time(time_stepping.get_next_time());
     scalar_field->update_boundary_conditions();
@@ -302,27 +335,12 @@ void Diffusion<dim>::solve(const unsigned int level)
     time_stepping.advance_time();
 
     // Snapshot stage, all time calls should be done with get_next_time()
-
-    if ((time_stepping.get_step_number() %
-          this->prm.terminal_output_frequency == 0) ||
-        (time_stepping.get_current_time() ==
-          time_stepping.get_end_time()))
-      postprocessing();
-
     if ((time_stepping.get_step_number() %
           this->prm.graphical_output_frequency == 0) ||
         (time_stepping.get_current_time() ==
           time_stepping.get_end_time()))
       output();
   }
-
-  Assert(time_stepping.get_current_time() == exact_solution->get_time(),
-    ExcMessage("Time mismatch between the time stepping class and the temperature function"));
-
-  convergence_table.update_table(level,
-                                 time_stepping.get_previous_step_size(),
-                                 parameters.convergence_test_parameters.test_type ==
-                                     ConvergenceTest::ConvergenceTestType::spatial);
 
   *this->pcout << std::endl << std::endl;
 }
@@ -332,69 +350,43 @@ void Diffusion<dim>::solve(const unsigned int level)
 template <int dim>
 void Diffusion<dim>::run()
 {
-  make_grid(parameters.spatial_discretization_parameters.n_initial_global_refinements);
+  make_grid(7);
 
-  switch (parameters.convergence_test_parameters.test_type)
+  for (unsigned int cycle = 0; cycle < 6; ++cycle)
   {
-  case ConvergenceTest::ConvergenceTestType::spatial:
-    for (unsigned int level = parameters.spatial_discretization_parameters.n_initial_global_refinements;
-         level < (parameters.spatial_discretization_parameters.n_initial_global_refinements +
-                  parameters.convergence_test_parameters.n_spatial_cycles);
-         ++level)
-    {
-      *this->pcout  << std::setprecision(1)
-                    << "Solving until t = "
-                    << std::fixed << time_stepping.get_end_time()
-                    << " with a refinement level of " << level
-                    << std::endl;
+    double time_step = parameters.time_discretization_parameters.initial_time_step *
+                       pow(parameters.convergence_test_parameters.step_size_reduction_factor,
+                           cycle);
 
-      time_stepping.restart();
+    *this->pcout  << std::setprecision(1)
+                  << "Solving until t = "
+                  << std::fixed << time_stepping.get_end_time()
+                  << " with a refinement level of "
+                  << parameters.spatial_discretization_parameters.n_initial_global_refinements
+                  << std::endl;
 
-      solve(level);
-
-      this->triangulation.refine_global();
-    }
-    break;
-  case ConvergenceTest::ConvergenceTestType::temporal:
-    for (unsigned int cycle = 0;
-         cycle < parameters.convergence_test_parameters.n_temporal_cycles;
-         ++cycle)
-    {
-      double time_step = parameters.time_discretization_parameters.initial_time_step *
-                         pow(parameters.convergence_test_parameters.step_size_reduction_factor,
-                             cycle);
-
-      *this->pcout  << std::setprecision(1)
-                    << "Solving until t = "
-                    << std::fixed << time_stepping.get_end_time()
-                    << " with a refinement level of "
-                    << parameters.spatial_discretization_parameters.n_initial_global_refinements
-                    << std::endl;
-
-      time_stepping.restart();
-
-      time_stepping.set_desired_next_step_size(time_step);
-
-      solve(parameters.spatial_discretization_parameters.n_initial_global_refinements);
-    }
-    break;
-  default:
-    break;
+    time_stepping.restart();
+    time_stepping.set_desired_next_step_size(time_step);
+    solve();
+    process_solution(cycle);
   }
 
-  *this->pcout << convergence_table;
+  convergence_table.set_precision("L2", 3);
+  convergence_table.set_precision("H1", 3);
+  convergence_table.set_precision("Linfty", 3);
+  convergence_table.set_scientific("L2", true);
+  convergence_table.set_scientific("H1", true);
+  convergence_table.set_scientific("Linfty", true);
+  convergence_table.omit_column_from_convergence_rate_evaluation("cycle");
+  convergence_table.omit_column_from_convergence_rate_evaluation("cells");
+  convergence_table.omit_column_from_convergence_rate_evaluation("dofs");
+  convergence_table.omit_column_from_convergence_rate_evaluation("time_step");
+  convergence_table.evaluate_all_convergence_rates("time_step", ConvergenceTable::RateMode::reduction_rate_log2);
 
-  std::ostringstream tablefilename;
-  tablefilename << ((parameters.convergence_test_parameters.test_type ==
-  									 ConvergenceTest::ConvergenceTestType::spatial)
-                     ? "Diffusion_SpatialTest"
-                     : ("Diffusion_TemporalTest_Level" + std::to_string(parameters.spatial_discretization_parameters.n_initial_global_refinements)))
-                << "_Pe"
-                << parameters.Pe;
-
-  convergence_table.write_text(tablefilename.str());
+  *this->pcout << std::endl;
+  if (this->pcout->is_active())
+    convergence_table.write_text(this->pcout->get_stream());
 }
-
 
 
 } // namespace RMHD
@@ -412,10 +404,8 @@ int main(int argc, char *argv[])
                                                         argv,
                                                         1);
 
-    RunTimeParameters::ProblemParameters parameter_set("Diffusion.prm", true);
-
+    RunTimeParameters::ProblemParameters parameter_set("Diffusion.prm");
     Diffusion<2> simulation(parameter_set);
-
     simulation.run();
   }
   catch(std::exception& exc)
