b76710c441ad05c12b1810607dc4f50249cf950a
Sebastian Glane <glane@tu-berlin.de>
2021-08-12 11:52:11 +0200

    Modified computation of error norms in ThermalTGV.cc

--- applications/ThermalTGV.cc
+++ applications/ThermalTGV.cc
@@ -7,13 +7,11 @@
 
 #include <deal.II/base/convergence_table.h>
 #include <deal.II/base/function.h>
-
 #include <deal.II/fe/mapping_q.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
-#include <rotatingMHD/convergence_test.h>
 
 #include <memory>
 
@@ -52,8 +50,7 @@ private:
 
   HeatEquation<dim>                             heat_equation;
 
-  ConvergenceAnalysisData<dim>                  convergence_table;
-  ConvergenceTest::ConvergenceTestData					convergence_data;
+  ConvergenceTable                              convergence_table;
 
   double                                        cfl_number;
 
@@ -67,11 +64,13 @@ private:
 
   void postprocessing();
 
+  void process_solution(const unsigned int cycle);
+
   void output();
 
   void update_entities();
 
-  void solve(const unsigned int &level);
+  void solve(const unsigned int level);
 };
 
 template <int dim>
@@ -98,8 +97,7 @@ heat_equation(parameters.heat_equation_parameters,
               velocity_exact_solution,
               this->mapping,
               this->pcout,
-              this->computing_timer),
-convergence_table(temperature, *temperature_exact_solution)
+              this->computing_timer)
 {
   *this->pcout << parameters << std::endl << std::endl;
 
@@ -202,6 +200,62 @@ void ThermalTGV<dim>::postprocessing()
            << time_stepping.get_next_step_size()  << std::endl;
 }
 
+
+template <int dim>
+void ThermalTGV<dim>::process_solution(const unsigned int cycle)
+{
+  const double current_time{time_stepping.get_current_time()};
+
+  using ExactSolution = EquationData::ThermalTGV::TemperatureExactSolution<dim>;
+
+  Vector<float> difference_per_cell(this->triangulation.n_active_cells());
+  VectorTools::integrate_difference(*temperature->dof_handler,
+                                    temperature->solution,
+                                    ExactSolution(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    QGauss<dim>(temperature->fe_degree + 1),
+                                    VectorTools::L2_norm);
+  const double L2_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::L2_norm);
+  VectorTools::integrate_difference(*temperature->dof_handler,
+                                    temperature->solution,
+                                    ExactSolution(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    QGauss<dim>(temperature->fe_degree + 1),
+                                    VectorTools::H1_seminorm);
+  const double H1_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::H1_seminorm);
+  const QTrapez<1>     q_trapez;
+  const QIterated<dim> q_iterated(q_trapez, temperature->fe_degree * 2 + 1);
+  VectorTools::integrate_difference(*temperature->dof_handler,
+                                    temperature->solution,
+                                    ExactSolution(parameters.Pe, current_time),
+                                    difference_per_cell,
+                                    q_iterated,
+                                    VectorTools::Linfty_norm);
+  const double Linfty_error =
+    VectorTools::compute_global_error(this->triangulation,
+                                      difference_per_cell,
+                                      VectorTools::Linfty_norm);
+
+  const unsigned int n_active_cells = this->triangulation.n_global_active_cells();
+  const unsigned int n_dofs         = temperature->dof_handler->n_dofs();
+
+  convergence_table.add_value("cycle", cycle);
+  convergence_table.add_value("time_step", time_stepping.get_previous_step_size());
+  convergence_table.add_value("cells", n_active_cells);
+  convergence_table.add_value("dofs", n_dofs);
+  convergence_table.add_value("L2", L2_error);
+  convergence_table.add_value("H1", H1_error);
+  convergence_table.add_value("Linfty", Linfty_error);
+
+}
+
+
 template <int dim>
 void ThermalTGV<dim>::output()
 {
@@ -240,7 +294,7 @@ void ThermalTGV<dim>::update_entities()
 }
 
 template <int dim>
-void ThermalTGV<dim>::solve(const unsigned int &level)
+void ThermalTGV<dim>::solve(const unsigned int /* level */)
 {
   setup_dofs();
   setup_constraints();
@@ -291,20 +345,9 @@ void ThermalTGV<dim>::solve(const unsigned int &level)
   }
 
   Assert(time_stepping.get_current_time() == temperature_exact_solution->get_time(),
-    ExcMessage("Time mismatch between the time stepping class and the temperature function"));
+         ExcMessage("Time mismatch between the time stepping class and the temperature function"));
 
-  {
-    auto error_map = temperature->compute_error(*temperature_exact_solution, this->mapping);
 
-    convergence_data.update_table(*this->temperature->dof_handler,
-                                  time_stepping.get_previous_step_size(),
-                                  error_map);
-  }
-  convergence_table.update_table(
-    level,
-    time_stepping.get_previous_step_size(),
-    parameters.convergence_test_parameters.test_type ==
-    		ConvergenceTest::ConvergenceTestType::spatial);
 
   log_file << "\n";
 
@@ -334,6 +377,8 @@ void ThermalTGV<dim>::run()
 
       solve(level);
 
+      process_solution(level);
+
       this->triangulation.refine_global();
     }
     break;
@@ -358,26 +403,29 @@ void ThermalTGV<dim>::run()
       time_stepping.set_desired_next_step_size(time_step);
 
       solve(parameters.spatial_discretization_parameters.n_initial_global_refinements);
+
+      process_solution(cycle);
     }
     break;
   default:
     break;
   }
 
-  *this->pcout << convergence_table;
+  convergence_table.set_precision("L2", 3);
+  convergence_table.set_precision("H1", 3);
+  convergence_table.set_precision("Linfty", 3);
+  convergence_table.set_scientific("L2", true);
+  convergence_table.set_scientific("H1", true);
+  convergence_table.set_scientific("Linfty", true);
+  convergence_table.omit_column_from_convergence_rate_evaluation("cycle");
+  convergence_table.omit_column_from_convergence_rate_evaluation("cells");
+  convergence_table.omit_column_from_convergence_rate_evaluation("dofs");
+  convergence_table.omit_column_from_convergence_rate_evaluation("time_step");
+  convergence_table.evaluate_all_convergence_rates("time_step", ConvergenceTable::RateMode::reduction_rate_log2);
 
-  std::ostringstream tablefilename;
-  tablefilename << ((parameters.convergence_test_parameters.test_type ==
-  									 ConvergenceTest::ConvergenceTestType::spatial)
-                     ? "ThermalTGV_SpatialTest"
-                     : ("ThermalTGV_TemporalTest_Level" + std::to_string(parameters.spatial_discretization_parameters.n_initial_global_refinements)))
-                << "_Pe"
-                << parameters.Pe;
-
-  convergence_table.write_text(tablefilename.str());
-
-  *this->pcout << convergence_data;
-  convergence_data.save("ThermalTGV_DumpTest.txt");
+  *this->pcout << std::endl;
+  if (this->pcout->is_active())
+    convergence_table.write_text(this->pcout->get_stream());
 }
 
 } // namespace RMHD
