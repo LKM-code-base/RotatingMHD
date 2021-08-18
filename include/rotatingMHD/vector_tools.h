1005ac57751d842a01583e4475c8c3c978a0dc42
Sebastian Glane <glane@tu-berlin.de>
2021-08-18 14:31:35 +0200

    Added documentation

--- include/rotatingMHD/vector_tools.h
+++ include/rotatingMHD/vector_tools.h
@@ -15,6 +15,10 @@ namespace VectorTools
 
 using NormType = dealii::VectorTools::NormType;
 
+/*!
+ * Computes the error of the solution specified by the finite element field with
+ * respect to the given exact solution.
+ */
 template <int dim, typename VectorType>
 std::map<NormType, double>
 compute_error
@@ -22,12 +26,19 @@ compute_error
  const Entities::FE_FieldBase<dim, VectorType>  &fe_field,
  const Function<dim>                            &exact_solution);
 
+/*!
+ * Same as @ref compute_error above but with a default mapping.
+ */
 template <int dim, typename VectorType>
 std::map<NormType, double>
 compute_error
 (const Entities::FE_FieldBase<dim, VectorType>  &fe_field,
  const Function<dim>                            &exact_solution);
 
+/*!
+ * Interpolates the function on the support points at the finite element field
+ * and applies the hanging node constraints.
+ */
 template <int dim, typename VectorType>
 void interpolate
 (const Mapping<dim>                             &mapping,
@@ -35,12 +46,19 @@ void interpolate
  const Function<dim>                            &function,
  VectorType                                     &vector);
 
+/*!
+ * Same as @ref interpolate above but with a default mapping.
+ */
 template <int dim, typename VectorType>
 void interpolate
 (const Entities::FE_FieldBase<dim, VectorType>  &fe_field,
  const Function<dim>                            &function,
  VectorType                                     &vector);
 
+/*!
+ * Projects the function to the finite element field and applies the constraints
+ * specified by the finite element field.
+ */
 template <int dim, typename VectorType>
 void project
 (const Mapping<dim>                             &mapping,
@@ -48,6 +66,9 @@ void project
  const Function<dim>                            &function,
  VectorType                                     &vector);
 
+/*!
+ * Same as @ref project above but with a default mapping.
+ */
 template <int dim, typename VectorType>
 void project
 (const Entities::FE_FieldBase<dim, VectorType>  &fe_field,
