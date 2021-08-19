3b4ae4534d3b85401c5daa4d541e30db501e6545
Sebastian Glane <glane@tu-berlin.de>
2021-08-19 14:32:34 +0200

    Move equation data objects of ThermalTGV convergence test to application file

--- convergence_tests/ThermalTGV.cc
+++ convergence_tests/ThermalTGV.cc
@@ -1,6 +1,5 @@
 #include <rotatingMHD/convergence_test.h>
 #include <rotatingMHD/convection_diffusion_solver.h>
-#include <rotatingMHD/equation_data.h>
 #include <rotatingMHD/finite_element_field.h>
 #include <rotatingMHD/problem_class.h>
 #include <rotatingMHD/run_time_parameters.h>
@@ -17,20 +16,181 @@
 
 #include <memory>
 
-namespace RMHD
+namespace ThermalTGV
 {
 
 using namespace dealii;
+using namespace RMHD;
+
+
+namespace EquationData
+{
+template <int dim>
+class VelocityExactSolution : public TensorFunction<1, dim>
+{
+public:
+  VelocityExactSolution(const double time = 0);
+
+  virtual Tensor<1, dim> value(const Point<dim>  &p) const override;
+
+private:
+  /*!
+   * @brief The wave number.
+   */
+  const double k = 2. * M_PI;
+};
+
+template <int dim>
+class TemperatureExactSolution : public Function<dim>
+{
+public:
+  TemperatureExactSolution(const double Pe,
+                           const double time = 0);
+
+  virtual double value(const Point<dim> &p,
+                       const unsigned int component = 0) const override;
+
+  virtual Tensor<1, dim> gradient(const Point<dim> &point,
+                                  const unsigned int = 0) const override;
+
+private:
+  /*!
+   * @brief The Peclet number.
+   */
+  const double Pe;
+
+  /*!
+   * @brief The wave number.
+   */
+  const double k = 2. * M_PI;
+};
+
+
+
+template <int dim>
+VelocityExactSolution<dim>::VelocityExactSolution
+(const double time)
+:
+TensorFunction<1, dim>(time)
+{}
+
+template <int dim>
+Tensor<1, dim> VelocityExactSolution<dim>::value
+(const Point<dim>  &point) const
+{
+  Tensor<1, dim>  return_value;
+
+  const double x = point(0);
+  const double y = point(1);
+
+  return_value[0] = cos(k * x) * cos(k * y);
+  return_value[1] = sin(k * x) * sin(k * y);
+
+  return return_value;
+}
+
+
+
+template <int dim>
+class VelocityField : public Function<dim>
+{
+public:
+  VelocityField(const double time = 0);
+
+  virtual void vector_value(const Point<dim>  &p,
+                            Vector<double>    &values) const override;
+private:
+
+  /*!
+   * @brief The wave number.
+   */
+  const double k = 2. * M_PI;
+};
+
+
+
+template <int dim>
+TemperatureExactSolution<dim>::TemperatureExactSolution
+(const double Pe,
+ const double time)
+:
+Function<dim>(1, time),
+Pe(Pe)
+{}
+
+
+
+template<int dim>
+double TemperatureExactSolution<dim>::value
+(const Point<dim> &point,
+ const unsigned int /* component */) const
+{
+  const double t = this->get_time();
+  const double x = point(0);
+  const double y = point(1);
+
+  const double F = exp(-2.0 * k * k  / Pe * t);
+
+  return (F *(cos(k * x) * sin(k * y)));
+}
+
+
+
+template<int dim>
+Tensor<1, dim> TemperatureExactSolution<dim>::gradient
+(const Point<dim> &point,
+ const unsigned int /* component */) const
+{
+  Tensor<1, dim>  return_value;
+  const double t = this->get_time();
+  const double x = point(0);
+  const double y = point(1);
+
+  const double F = exp(-2.0 * k * k  / Pe * t);
+
+  return_value[0] = - F * k * sin(k * x) * sin(k * y);
+  return_value[1] = + F * k * cos(k * x) * cos(k * y);
+
+  return return_value;
+}
+
+
+
+template <int dim>
+VelocityField<dim>::VelocityField
+(const double time)
+:
+Function<dim>(dim, time)
+{}
+
+
+
+template <int dim>
+void VelocityField<dim>::vector_value
+(const Point<dim>  &point,
+ Vector<double>    &values) const
+{
+  const double x = point(0);
+  const double y = point(1);
+
+  values[0] = cos(k * x) * cos(k * y);
+  values[1] = sin(k * x) * sin(k * y);
+}
+
+} // namespace EquationData
+
 
 /*!
  * @class ThermalTGV
+ *
  * @todo Add documentation
+ *
  */
 template <int dim>
-class ThermalTGV : public Problem<dim>
+class ThermalTGVProblem : public Problem<dim>
 {
 public:
-  ThermalTGV(const RunTimeParameters::ProblemParameters &parameters);
+  ThermalTGVProblem(const RunTimeParameters::ProblemParameters &parameters);
 
   void run();
 
@@ -41,8 +201,6 @@ private:
   std::ofstream                                 log_file;
 
   std::shared_ptr<Entities::FE_ScalarField<dim>>  temperature;
-
-  LinearAlgebra::MPI::Vector                    error;
 
   TimeDiscretization::VSIMEXMethod              time_stepping;
 
@@ -75,7 +233,7 @@ private:
 };
 
 template <int dim>
-ThermalTGV<dim>::ThermalTGV(
+ThermalTGVProblem<dim>::ThermalTGVProblem(
   const RunTimeParameters::ProblemParameters &parameters)
 :
 Problem<dim>(parameters),
@@ -86,11 +244,11 @@ temperature(std::make_shared<Entities::FE_ScalarField<dim>>(parameters.fe_degree
                                                           "Temperature")),
 time_stepping(parameters.time_discretization_parameters),
 temperature_exact_solution(
-  std::make_shared<EquationData::ThermalTGV::TemperatureExactSolution<dim>>(
+  std::make_shared<EquationData::TemperatureExactSolution<dim>>(
     parameters.Pe,
     parameters.time_discretization_parameters.start_time)),
 velocity_exact_solution(
-  std::make_shared<EquationData::ThermalTGV::VelocityExactSolution<dim>>(
+  std::make_shared<EquationData::VelocityExactSolution<dim>>(
     parameters.time_discretization_parameters.start_time)),
 heat_equation(parameters.heat_equation_parameters,
               time_stepping,
@@ -110,7 +268,7 @@ convergence_table(temperature, *temperature_exact_solution)
 }
 
 template <int dim>
-void ThermalTGV<dim>::
+void ThermalTGVProblem<dim>::
 make_grid(const unsigned int &n_global_refinements)
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Setup - Triangulation");
@@ -141,7 +299,7 @@ make_grid(const unsigned int &n_global_refinements)
 }
 
 template <int dim>
-void ThermalTGV<dim>::setup_dofs()
+void ThermalTGVProblem<dim>::setup_dofs()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Setup - DoFs");
 
@@ -155,7 +313,7 @@ void ThermalTGV<dim>::setup_dofs()
 }
 
 template <int dim>
-void ThermalTGV<dim>::setup_constraints()
+void ThermalTGVProblem<dim>::setup_constraints()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Setup - Boundary conditions");
 
@@ -173,7 +331,7 @@ void ThermalTGV<dim>::setup_constraints()
 }
 
 template <int dim>
-void ThermalTGV<dim>::initialize()
+void ThermalTGVProblem<dim>::initialize()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Setup - Initial conditions");
 
@@ -183,7 +341,7 @@ void ThermalTGV<dim>::initialize()
 }
 
 template <int dim>
-void ThermalTGV<dim>::postprocessing()
+void ThermalTGVProblem<dim>::postprocessing()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Postprocessing");
 
@@ -205,7 +363,7 @@ void ThermalTGV<dim>::postprocessing()
 }
 
 template <int dim>
-void ThermalTGV<dim>::output()
+void ThermalTGVProblem<dim>::output()
 {
   TimerOutput::Scope  t(*this->computing_timer, "Problem: Graphical output");
 
@@ -214,10 +372,6 @@ void ThermalTGV<dim>::output()
   data_out.add_data_vector(temperature->get_dof_handler(),
                            temperature->solution,
                            "temperature");
-  data_out.add_data_vector(temperature->get_dof_handler(),
-                           error,
-                           "error");
-
   data_out.build_patches(temperature->fe_degree());
 
   static int out_index = 0;
@@ -232,18 +386,17 @@ void ThermalTGV<dim>::output()
 }
 
 template <int dim>
-void ThermalTGV<dim>::update_entities()
+void ThermalTGVProblem<dim>::update_entities()
 {
   temperature->update_solution_vectors();
 }
 
 template <int dim>
-void ThermalTGV<dim>::solve(const unsigned int &level)
+void ThermalTGVProblem<dim>::solve(const unsigned int &level)
 {
   setup_dofs();
   setup_constraints();
   temperature->setup_vectors();
-  error.reinit(temperature->solution);
   initialize();
 
   // Outputs the fields at t_0, i.e. the initial conditions.
@@ -309,7 +462,7 @@ void ThermalTGV<dim>::solve(const unsigned int &level)
 }
 
 template <int dim>
-void ThermalTGV<dim>::run()
+void ThermalTGVProblem<dim>::run()
 {
   make_grid(parameters.spatial_discretization_parameters.n_initial_global_refinements);
 
@@ -377,14 +530,14 @@ void ThermalTGV<dim>::run()
   convergence_data.save("ThermalTGV_DumpTest.txt");
 }
 
-} // namespace RMHD
+} // namespace ThermalTGV
 
 int main(int argc, char *argv[])
 {
   try
   {
       using namespace dealii;
-      using namespace RMHD;
+      using namespace ThermalTGV;
 
       Utilities::MPI::MPI_InitFinalize mpi_initialization(
         argc, argv, 1);
@@ -392,10 +545,8 @@ int main(int argc, char *argv[])
       RunTimeParameters::ProblemParameters parameter_set("ThermalTGV.prm",
                                                          true);
 
-      ThermalTGV<2> simulation(parameter_set);
-
+      ThermalTGVProblem<2> simulation(parameter_set);
       simulation.run();
-
 
   }
   catch (std::exception &exc)
--- source/equation_data.cc
+++ source/equation_data.cc
@@ -26,89 +26,6 @@ namespace Couette
 namespace ThermalTGV
 {
 
-template <int dim>
-VelocityExactSolution<dim>::VelocityExactSolution
-(const double time)
-:
-TensorFunction<1, dim>(time)
-{}
-
-template <int dim>
-Tensor<1, dim> VelocityExactSolution<dim>::value
-(const Point<dim>  &point) const
-{
-  Tensor<1, dim>  return_value;
-
-  const double x = point(0);
-  const double y = point(1);
-
-  return_value[0] = cos(k * x) * cos(k * y);
-  return_value[1] = sin(k * x) * sin(k * y);
-
-  return return_value;
-}
-
-
-template <int dim>
-TemperatureExactSolution<dim>::TemperatureExactSolution
-(const double Pe,
- const double time)
-:
-Function<dim>(1, time),
-Pe(Pe)
-{}
-
-template<int dim>
-double TemperatureExactSolution<dim>::value
-(const Point<dim> &point,
- const unsigned int /* component */) const
-{
-  const double t = this->get_time();
-  const double x = point(0);
-  const double y = point(1);
-
-  const double F = exp(-2.0 * k * k  / Pe * t);
-
-  return (F *(cos(k * x) * sin(k * y)));
-}
-
-template<int dim>
-Tensor<1, dim> TemperatureExactSolution<dim>::gradient
-(const Point<dim> &point,
- const unsigned int /* component */) const
-{
-  Tensor<1, dim>  return_value;
-  const double t = this->get_time();
-  const double x = point(0);
-  const double y = point(1);
-
-  const double F = exp(-2.0 * k * k  / Pe * t);
-
-  return_value[0] = - F * k * sin(k * x) * sin(k * y);
-  return_value[1] = + F * k * cos(k * x) * cos(k * y);
-
-  return return_value;
-}
-
-template <int dim>
-VelocityField<dim>::VelocityField
-(const double time)
-:
-Function<dim>(dim, time)
-{}
-
-template <int dim>
-void VelocityField<dim>::vector_value
-(const Point<dim>  &point,
- Vector<double>    &values) const
-{
-  const double x = point(0);
-  const double y = point(1);
-
-  values[0] = cos(k * x) * cos(k * y);
-  values[1] = sin(k * x) * sin(k * y);
-}
-
 } // namespace ThermalTGV
 
 } // namespace EquationData
@@ -117,11 +34,3 @@ void VelocityField<dim>::vector_value
 
 
 // explicit instantiation
-template class RMHD::EquationData::ThermalTGV::VelocityExactSolution<2>;
-template class RMHD::EquationData::ThermalTGV::VelocityExactSolution<3>;
-
-template class RMHD::EquationData::ThermalTGV::TemperatureExactSolution<2>;
-template class RMHD::EquationData::ThermalTGV::TemperatureExactSolution<3>;
-
-template class RMHD::EquationData::ThermalTGV::VelocityField<2>;
-template class RMHD::EquationData::ThermalTGV::VelocityField<3>;
