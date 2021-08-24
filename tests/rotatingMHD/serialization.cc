fd8ce225d18e8eafbbb87f60ecc067d021c4071a
Sebastian Glane <glane@tu-berlin.de>
2021-08-24 13:55:11 +0200

    Added MPI test

--- tests/rotatingMHD/serialization.cc
+++ tests/rotatingMHD/serialization.cc
@@ -1,3 +1,4 @@
+#include <deal.II/base/conditional_ostream.h>
 #include <deal.II/base/function_lib.h>
 #include <deal.II/base/mpi.h>
 #include <deal.II/distributed/tria.h>
@@ -8,15 +9,22 @@
 #include <rotatingMHD/problem_class.h>
 #include <rotatingMHD/vector_tools.h>
 
-namespace RMHD
-{
+#include <array>
+
+// Test serialization in parallel
+
+using namespace dealii;
+
+using namespace RMHD;
 
 template<int dim>
-void serialize_scalar_field()
+double serialize_scalar_field()
 {
   parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
 
-  Entities::FE_ScalarField<dim, LinearAlgebra::MPI::Vector>
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_ScalarField<dim, VectorType>
   field(1, tria, "Scalar Field");
 
   Functions::SquareFunction<dim> function;
@@ -31,26 +39,31 @@ void serialize_scalar_field()
   field.close_boundary_conditions(false);
   field.apply_boundary_conditions(false);
 
-  VectorTools::project(field,
-                       function,
-                       field.solution);
-  VectorTools::interpolate(field,
-                           function,
-                           field.old_solution);
+  RMHD::VectorTools::project(field,
+                             function,
+                             field.solution);
 
   SolutionTransferContainer<dim>  container;
   container.add_entity(field, false);
   container.serialize("scalar_field.mesh");
+
+  VectorType  distributed_solution(field.distributed_vector);
+  distributed_solution = field.solution;
+
+  return(distributed_solution.l2_norm());
+
 }
 
 
 
 template<int dim>
-void deserialize_scalar_field()
+double deserialize_scalar_field()
 {
   parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
 
-  Entities::FE_ScalarField<dim, LinearAlgebra::MPI::Vector>
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_ScalarField<dim, VectorType>
   field(1, tria, "Scalar Field");
 
   GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
@@ -59,31 +72,211 @@ void deserialize_scalar_field()
   container.add_entity(field, false);
   container.deserialize(tria, "scalar_field.mesh");
 
-  Point<dim> point;
-  for (unsigned d=0; d<dim; ++d)
-    point[d] = 0.5;
+  VectorType  distributed_solution(field.distributed_vector);
+  distributed_solution = field.solution;
 
-  Functions::SquareFunction<dim> function;
-  const double function_value{function.value(point)};
+  return (distributed_solution.l2_norm());
 
-  std::cout << std::fabs(field.point_value(point) - function_value) << std::endl;
 }
 
 
-}  // namespace RMHD
+
+template<int dim>
+double serialize_vector_field()
+{
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_VectorField<dim, VectorType>
+  field(2, tria, "Vector Field");
+
+  GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
+  tria.refine_global(3);
+
+  field.setup_dofs();
+  field.setup_vectors();
+
+  field.setup_boundary_conditions();
+  field.close_boundary_conditions(false);
+  field.apply_boundary_conditions(false);
+
+  Functions::CosineFunction<dim> function(dim);
+
+  RMHD::VectorTools::project(field,
+                             function,
+                             field.solution);
+
+  SolutionTransferContainer<dim>  container;
+  container.add_entity(field, false);
+  container.serialize("vector_field.mesh");
+
+  VectorType  distributed_solution(field.distributed_vector);
+  distributed_solution = field.solution;
+
+  return (distributed_solution.l2_norm());
+
+}
+
+
+
+template<int dim>
+double deserialize_vector_field()
+{
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_VectorField<dim, VectorType>
+  field(2, tria, "Vector Field");
+
+  GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
+
+  SolutionTransferContainer<dim>  container;
+  container.add_entity(field, false);
+  container.deserialize(tria, "vector_field.mesh");
+
+  VectorType  distributed_solution(field.distributed_vector);
+  distributed_solution = field.solution;
+
+  return (distributed_solution.l2_norm());
+}
+
+
+
+template<int dim>
+std::array<double, 2> serialize_field_collection()
+{
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_ScalarField<dim, VectorType>
+  scalar_field(1, tria, "Scalar Field");
+
+  Entities::FE_VectorField<dim, VectorType>
+  vector_field(2, tria, "Vector Field");
+
+  GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
+  tria.refine_global(3);
+
+  scalar_field.setup_dofs();
+  scalar_field.setup_vectors();
+
+  scalar_field.setup_boundary_conditions();
+  scalar_field.close_boundary_conditions(false);
+  scalar_field.apply_boundary_conditions(false);
+
+  Functions::SquareFunction<dim> scalar_function;
+  RMHD::VectorTools::project(scalar_field,
+                             scalar_function,
+                             scalar_field.solution);
+
+  vector_field.setup_dofs();
+  vector_field.setup_vectors();
+
+  vector_field.setup_boundary_conditions();
+  vector_field.close_boundary_conditions(false);
+  vector_field.apply_boundary_conditions(false);
+
+  Functions::CosineFunction<dim> vector_function(dim);
+  RMHD::VectorTools::project(vector_field,
+                             vector_function,
+                             vector_field.solution);
+
+
+  SolutionTransferContainer<dim>  container;
+  container.add_entity(scalar_field, false);
+  container.add_entity(vector_field, false);
+  container.serialize("field_collection.mesh");
+
+  std::array<double, 2> norms;
+  VectorType  distributed_solution(scalar_field.distributed_vector);
+  distributed_solution = scalar_field.solution;
+  norms[0] = distributed_solution.l2_norm();
+
+  distributed_solution.reinit(vector_field.distributed_vector);
+  distributed_solution = vector_field.solution;
+  norms[1] = distributed_solution.l2_norm();
+
+  return (norms);
+
+}
+
+
+
+template<int dim>
+std::array<double, 2> deserialize_field_collection()
+{
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+
+  using VectorType = RMHD::LinearAlgebra::MPI::Vector;
+
+  Entities::FE_ScalarField<dim, VectorType>
+  scalar_field(1, tria, "Scalar Field");
+
+  Entities::FE_VectorField<dim, VectorType>
+  vector_field(2, tria, "Vector Field");
+
+  GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
+
+  SolutionTransferContainer<dim>  container;
+  container.add_entity(scalar_field, false);
+  container.add_entity(vector_field, false);
+  container.deserialize(tria, "field_collection.mesh");
+
+  std::array<double, 2> norms;
+  VectorType  distributed_solution(scalar_field.distributed_vector);
+  distributed_solution = scalar_field.solution;
+  norms[0] = distributed_solution.l2_norm();
+
+  distributed_solution.reinit(vector_field.distributed_vector);
+  distributed_solution = vector_field.solution;
+  norms[1] = distributed_solution.l2_norm();
+
+  return (norms);
+
+}
+
+
 
 int main(int argc, char *argv[])
 {
   try
   {
-    dealii::Utilities::MPI::MPI_InitFinalize  mpi_initialization(argc, argv);
-    dealii::deallog.depth_console(0);
+    Utilities::MPI::MPI_InitFinalize  mpi_initialization(argc, argv, 1);
+    ConditionalOStream  pcout(std::cout,
+                              Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0);
 
-    RMHD::serialize_scalar_field<2>();
-    RMHD::deserialize_scalar_field<2>();
+    {
+      double norm[2];
+      norm[0] = serialize_scalar_field<2>();
+      norm[1] = deserialize_scalar_field<2>();
+      pcout << std::scientific << std::fabs(norm[0] - norm[1]) << std::endl;
 
-    RMHD::serialize_scalar_field<3>();
-    RMHD::deserialize_scalar_field<3>();
+      norm[0] = serialize_scalar_field<3>();
+      norm[1] = deserialize_scalar_field<3>();
+      pcout << std::scientific << std::fabs(norm[0] - norm[1])  << std::endl;
+
+      norm[0] = serialize_vector_field<2>();
+      norm[1] = deserialize_vector_field<2>();
+      pcout << std::scientific << std::fabs(norm[0] - norm[1]) << std::endl;
+
+      norm[0] = serialize_vector_field<3>();
+      norm[1] = deserialize_vector_field<3>();
+      pcout << std::scientific << std::fabs(norm[0] - norm[1]) << std::endl;
+
+      norm[0] = serialize_vector_field<2>();
+      norm[1] = deserialize_vector_field<2>();
+      pcout << std::scientific << std::fabs(norm[0] - norm[1]) << std::endl;
+    }
+    {
+      std::array<double, 2> norm[2];
+      norm[0] = serialize_field_collection<3>();
+      norm[1] = deserialize_field_collection<3>();
+      pcout << std::scientific << std::fabs(norm[0][0] - norm[1][0]) << std::endl;
+      pcout << std::scientific << std::fabs(norm[0][1] - norm[1][1]) << std::endl;
+    }
   }
   catch(std::exception & exc)
   {
