91b48d62660c7bf84d658b51d11d1733b6d84fad
Sebastian Glane <glane@tu-berlin.de>
2021-08-27 11:26:29 +0200

    Add point_value method to tests

--- tests/rotatingMHD/finite_element_field_01.cc
+++ tests/rotatingMHD/finite_element_field_01.cc
@@ -1,4 +1,5 @@
 #include <rotatingMHD/finite_element_field.h>
+#include <rotatingMHD/vector_tools.h>
 
 #include <deal.II/base/function_lib.h>
 #include <deal.II/base/tensor_function.h>
@@ -15,10 +16,10 @@ void test_scalar_field()
   Triangulation<dim>  tria;
 
   Entities::FE_ScalarField<dim, Vector<double>> field_01(1, tria, "Scalar Field 01");
-  Entities::FE_ScalarField<dim, Vector<double>> field_02(field_01, "Scalar Field 02");
 
   GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
 
+  // Test setup
   field_01.setup_dofs();
   field_01.setup_vectors();
 
@@ -27,6 +28,7 @@ void test_scalar_field()
   std::shared_ptr<Function<dim>>  ptr02 =
       std::make_shared<Functions::CutOffFunctionC1<dim>>();
 
+  // Test assignment of boundary conditions
   field_01.setup_boundary_conditions();
   field_01.set_periodic_boundary_condition(0, 1, 0);
   field_01.set_dirichlet_boundary_condition(2, ptr01, true);
@@ -34,24 +36,47 @@ void test_scalar_field()
   field_01.close_boundary_conditions(true);
   field_01.apply_boundary_conditions(true);
 
+  // Test copying and modification of boundary conditions
+  Entities::FE_ScalarField<dim, Vector<double>> field_02(field_01, "Scalar Field 02");
   field_02.setup_dofs();
   field_02.setup_vectors();
   field_02.setup_boundary_conditions();
-
   Entities::BoundaryConditionsBase<dim> &bc_02 = field_02.get_boundary_conditions();
   const Entities::BoundaryConditionsBase<dim> &bc_01 = field_01.get_boundary_conditions();
-
   bc_02.copy(bc_01);
   for (auto &dirichlet_bc: bc_02.dirichlet_bcs)
     dirichlet_bc.second = std::make_shared<Functions::ZeroFunction<dim>>();
-
   const std::vector<types::boundary_id> unconstrained_boundary_ids =
       bc_02.get_unconstrained_boundary_ids();
   for (const auto id: unconstrained_boundary_ids)
     field_02.set_neumann_boundary_condition(id, ptr01, true);
   field_02.close_boundary_conditions(true);
   field_02.apply_boundary_conditions(true);
+
+  // Test point_value
+  tria.refine_global(3);
+  Entities::FE_ScalarField<dim, Vector<double>> field_03(1, tria, "Scalar Field 03");
+  field_03.setup_dofs();
+  field_03.setup_vectors();
+  field_03.setup_boundary_conditions();
+  field_03.close_boundary_conditions(false);
+  field_03.apply_boundary_conditions(false);
+
+  const Functions::SquareFunction<dim> function;
+  Point<dim> point;
+  for (unsigned d=0; d<dim; ++d)
+    point[d] = 0.5;
+  const double function_value{function.value(point)};
+
+  RMHD::VectorTools::interpolate(field_03,
+                                 function,
+                                 field_03.solution);
+  std::cout << "Difference < 2e-15 ? "
+            << std::boolalpha
+            << bool(std::fabs(field_03.point_value(point) - function_value) < 2e-15)
+            << std::endl;
 }
+
 
 
 template<int dim>
@@ -62,11 +87,9 @@ void test_scalar_field_ptr()
   std::shared_ptr<Entities::FE_ScalarField<dim, Vector<double>>> field_01 =
       std::make_shared<Entities::FE_ScalarField<dim, Vector<double>>>(1, tria, "Scalar Field 01");
 
-  std::shared_ptr<Entities::FE_ScalarField<dim, Vector<double>>> field_02 =
-      std::make_shared<Entities::FE_ScalarField<dim, Vector<double>>>(*field_01, "Scalar Field 02");
-
   GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
 
+  // Test setup
   field_01->setup_dofs();
   field_01->setup_vectors();
 
@@ -75,6 +98,7 @@ void test_scalar_field_ptr()
   std::shared_ptr<Function<dim>>  ptr02 =
       std::make_shared<Functions::CutOffFunctionC1<dim>>();
 
+  // Test assignment of boundary conditions
   field_01->setup_boundary_conditions();
   field_01->set_periodic_boundary_condition(0, 1, 0);
   field_01->set_dirichlet_boundary_condition(2, ptr01, true);
@@ -82,25 +106,50 @@ void test_scalar_field_ptr()
   field_01->close_boundary_conditions(true);
   field_01->apply_boundary_conditions(true);
 
+  // Test copying and modification of boundary conditions
+  std::shared_ptr<Entities::FE_ScalarField<dim, Vector<double>>> field_02 =
+      std::make_shared<Entities::FE_ScalarField<dim, Vector<double>>>(*field_01, "Scalar Field 02");
   field_02->setup_dofs();
   field_02->setup_vectors();
   field_02->setup_boundary_conditions();
-
   Entities::BoundaryConditionsBase<dim> &bc_02 = field_02->get_boundary_conditions();
   const Entities::BoundaryConditionsBase<dim> &bc_01 = field_01->get_boundary_conditions();
-
   bc_02.copy(bc_01);
   for (auto &dirichlet_bc: bc_02.dirichlet_bcs)
     dirichlet_bc.second = std::make_shared<Functions::ZeroFunction<dim>>();
-
   const std::vector<types::boundary_id> unconstrained_boundary_ids =
       bc_02.get_unconstrained_boundary_ids();
   for (const auto id: unconstrained_boundary_ids)
     field_02->set_neumann_boundary_condition(id, ptr01, true);
-
   field_02->close_boundary_conditions(true);
   field_02->apply_boundary_conditions(true);
+
+  // Test point_value
+  tria.refine_global(3);
+  std::shared_ptr<Entities::FE_ScalarField<dim, Vector<double>>> field_03 =
+       std::make_shared<Entities::FE_ScalarField<dim, Vector<double>>>(1, tria, "Scalar Field 03");
+  field_03->setup_dofs();
+  field_03->setup_vectors();
+  field_03->setup_boundary_conditions();
+  field_03->close_boundary_conditions(false);
+  field_03->apply_boundary_conditions(false);
+
+  const Functions::SquareFunction<dim> function;
+
+  Point<dim> point;
+  for (unsigned d=0; d<dim; ++d)
+    point[d] = 0.5;
+  const double function_value{function.value(point)};
+
+  RMHD::VectorTools::interpolate(*field_03,
+                                 function,
+                                 field_03->solution);
+  std::cout << "Difference < 2e-15 ? "
+            << std::boolalpha
+            << bool(std::fabs(field_03->point_value(point) - function_value) < 2e-15)
+            << std::endl;
 }
+
 
 
 template<int dim>
@@ -108,10 +157,9 @@ void test_vector_field()
 {
   Triangulation<dim>  tria;
   Entities::FE_VectorField<dim, Vector<double>> field_01(1, tria, "Vector Field 01");
-  Entities::FE_VectorField<dim, Vector<double>> field_02(field_01, "Vector Field 02");
-
   GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
 
+  // Test setup
   field_01.setup_dofs();
   field_01.setup_vectors();
 
@@ -124,6 +172,7 @@ void test_vector_field()
   std::shared_ptr<Function<dim>>  ptr04 =
       std::make_shared<Functions::ConstantFunction<dim>>(1.0, dim);
 
+  // Test assignment of boundary conditions
   field_01.setup_boundary_conditions();
   field_01.set_periodic_boundary_condition(0, 1, 0);
   field_01.set_dirichlet_boundary_condition(2, ptr01, true);
@@ -133,28 +182,55 @@ void test_vector_field()
     field_01.set_tangential_component_boundary_condition(4, ptr03, true);
     field_01.set_normal_component_boundary_condition(5, ptr04, true);
   }
-
   field_01.close_boundary_conditions(true);
   field_01.apply_boundary_conditions(true);
 
+  // Test copying and modification of boundary conditions
+  Entities::FE_VectorField<dim, Vector<double>> field_02(field_01, "Vector Field 02");
   field_02.setup_dofs();
   field_02.setup_vectors();
   field_02.setup_boundary_conditions();
-
   Entities::BoundaryConditionsBase<dim> &bc_02 = field_02.get_boundary_conditions();
   const Entities::BoundaryConditionsBase<dim> &bc_01 = field_01.get_boundary_conditions();
-
   bc_02.copy(bc_01);
   for (auto &dirichlet_bc: bc_02.dirichlet_bcs)
     dirichlet_bc.second = std::make_shared<Functions::ZeroFunction<dim>>(dim);
-
-
   const std::vector<types::boundary_id> unconstrained_boundary_ids =
       bc_02.get_unconstrained_boundary_ids();
   for (const auto id: unconstrained_boundary_ids)
     field_02.set_neumann_boundary_condition(id, ptr02, true);
   field_02.close_boundary_conditions(true);
   field_02.apply_boundary_conditions(true);
+
+  // Test point_value
+  tria.refine_global(3);
+  Entities::FE_VectorField<dim, Vector<double>> field_03(2, tria, "Vector Field 03");
+  field_03.setup_dofs();
+  field_03.setup_vectors();
+  field_03.setup_boundary_conditions();
+  field_03.close_boundary_conditions(false);
+  field_03.apply_boundary_conditions(false);
+
+  const Functions::CosineFunction<dim> function(dim);
+  Point<dim> point;
+  for (unsigned d=0; d<dim; ++d)
+    point[d] = 0.5;
+  Vector<double> function_value(function.n_components);
+  function.vector_value(point,
+                        function_value);
+
+  RMHD::VectorTools::interpolate(field_03,
+                                 function,
+                                 field_03.solution);
+  Tensor<1, dim>  difference;
+  for (unsigned int d=0; d<dim; ++d)
+    difference[d] = function_value[d];
+
+  difference -= field_03.point_value(point);
+  std::cout << "Difference < 2e-15 ? "
+            << std::boolalpha
+            << bool(difference.norm() < 2e-15)
+            << std::endl;
 }
 
 
@@ -166,11 +242,9 @@ void test_vector_field_ptr()
   std::shared_ptr<Entities::FE_VectorField<dim, Vector<double>>> field_01 =
       std::make_shared<Entities::FE_VectorField<dim, Vector<double>>>(1, tria, "Vector Field 01");
 
-  std::shared_ptr<Entities::FE_VectorField<dim, Vector<double>>> field_02 =
-      std::make_shared<Entities::FE_VectorField<dim, Vector<double>>>(*field_01, "Vector Field 02");
-
   GridGenerator::hyper_cube(tria, 0.0, 1.0, true);
 
+  // Test setup
   field_01->setup_dofs();
   field_01->setup_vectors();
 
@@ -183,8 +257,8 @@ void test_vector_field_ptr()
   std::shared_ptr<Function<dim>>  ptr04 =
       std::make_shared<Functions::ConstantFunction<dim>>(1.0, dim);
 
+  // Test assignment of boundary conditions
   field_01->setup_boundary_conditions();
-  field_01->set_periodic_boundary_condition(0, 1, 0);
   field_01->set_periodic_boundary_condition(0, 1, 0);
   field_01->set_dirichlet_boundary_condition(2, ptr01, true);
   field_01->set_neumann_boundary_condition(3, ptr02);
@@ -196,24 +270,53 @@ void test_vector_field_ptr()
   field_01->close_boundary_conditions(true);
   field_01->apply_boundary_conditions(true);
 
+  // Test copying and modification of boundary conditions
+  std::shared_ptr<Entities::FE_VectorField<dim, Vector<double>>> field_02 =
+      std::make_shared<Entities::FE_VectorField<dim, Vector<double>>>(*field_01, "Vector Field 02");
   field_02->setup_dofs();
   field_02->setup_vectors();
   field_02->setup_boundary_conditions();
-
   Entities::BoundaryConditionsBase<dim> &bc_02 = field_02->get_boundary_conditions();
   const Entities::BoundaryConditionsBase<dim> &bc_01 = field_01->get_boundary_conditions();
-
   bc_02.copy(bc_01);
   for (auto &dirichlet_bc: bc_02.dirichlet_bcs)
     dirichlet_bc.second = std::make_shared<Functions::ZeroFunction<dim>>(dim);
-
   const std::vector<types::boundary_id> unconstrained_boundary_ids =
       bc_02.get_unconstrained_boundary_ids();
   for (const auto id: unconstrained_boundary_ids)
     field_02->set_neumann_boundary_condition(id, ptr02, true);
-
   field_02->close_boundary_conditions(true);
   field_02->apply_boundary_conditions(true);
+
+  // Test point_value
+  tria.refine_global(3);
+  std::shared_ptr<Entities::FE_VectorField<dim, Vector<double>>> field_03 =
+      std::make_shared<Entities::FE_VectorField<dim, Vector<double>>>(2, tria, "Vector Field 03");
+  field_03->setup_dofs();
+  field_03->setup_vectors();
+  field_03->setup_boundary_conditions();
+  field_03->close_boundary_conditions(false);
+  field_03->apply_boundary_conditions(false);
+
+  const Functions::CosineFunction<dim> function(dim);
+  Point<dim> point;
+  for (unsigned d=0; d<dim; ++d)
+    point[d] = 0.5;
+  Vector<double> function_value(function.n_components);
+  function.vector_value(point,
+                        function_value);
+
+  RMHD::VectorTools::interpolate(*field_03,
+                                 function,
+                                 field_03->solution);
+  Tensor<1, dim>  difference;
+  for (unsigned int d=0; d<dim; ++d)
+    difference[d] = function_value[d];
+  difference -= field_03->point_value(point);
+  std::cout << "Difference < 2e-15 ? "
+            << std::boolalpha
+            << bool(difference.norm() < 2e-15)
+            << std::endl;
 }
 
 
@@ -232,9 +335,9 @@ int main(void)
 
     test_vector_field<2>();
     test_vector_field<3>();
-//
-//    test_vector_field_ptr<2>();
-//    test_vector_field_ptr<3>();
+
+    test_vector_field_ptr<2>();
+    test_vector_field_ptr<3>();
   }
   catch(std::exception & exc)
   {
