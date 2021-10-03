5a5fcce76f02a6801b70c355828b815693ed0c64
j507 <jose.a.lara.h@gmail.com>
2021-10-03 23:25:45 +0200

    Added definition of assembly methods for the advection matrix, the projection step and the zeroth step

--- source/magnetic_induction/assembly_advection_matrix.cc
+++ source/magnetic_induction/assembly_advection_matrix.cc
@@ -1,5 +1,9 @@
 #include <rotatingMHD/magnetic_induction.h>
 
+#include <deal.II/base/work_stream.h>
+#include <deal.II/fe/fe_nothing.h>
+#include <deal.II/grid/filtered_iterator.h>
+
 
 
 namespace RMHD
@@ -15,7 +19,233 @@ namespace Solvers
 template <int dim>
 void MagneticInduction<dim>::assemble_advection_matrix()
 {
+  if (parameters.verbose)
+    *this->pcout << "  Magnetic induction: Assembling advection matrix...";
 
+  TimerOutput::Scope  t(*this->computing_timer, "Magnetic induction: Advection matrix assembly");
+
+  // Reset data
+  this->diffusion_step_advection_matrix = 0.;
+
+  // Dummy finite element for when the velocity is given by a function
+  const FESystem<dim> dummy_fe_system(FE_Nothing<dim>(1), dim);
+
+  // Create pointer to the pertinent finite element
+  const FiniteElement<dim>* const velocity_fe =
+              (velocity != nullptr) ?
+                &velocity->get_finite_element() :
+                &dummy_fe_system;
+
+  // Initiate the quadrature formula for exact numerical integration
+  const QGauss<dim>   quadrature_formula(magnetic_field->fe_degree() + 1);
+
+  // Set up the lambda function for the local assembly operation
+  using Scratch = typename AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<dim>;
+  auto worker =
+    [this](const typename DoFHandler<dim>::active_cell_iterator &cell,
+           AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<dim> &scratch,
+           AssemblyData::MagneticInduction::AdvectionMatrix::Copy         &data)
+    {
+      this->assemble_local_advection_matrix(cell,
+                                                     scratch,
+                                                     data);
+    };
+
+  // Set up the lambda function for the copy local to global operation
+  auto copier =
+    [this](const AssemblyData::MagneticInduction::AdvectionMatrix::Copy &data)
+    {
+      this->copy_local_to_global_advection_matrix(data);
+    };
+
+  // Assemble using the WorkStream approach
+  using CellFilter =
+    FilteredIterator<typename DoFHandler<dim>::active_cell_iterator>;
+
+  const UpdateFlags advection_update_flags = update_values|
+                                             update_gradients|
+                                             update_JxW_values;
+  WorkStream::run
+  (CellFilter(IteratorFilters::LocallyOwnedCell(),
+              magnetic_field->get_dof_handler().begin_active()),
+   CellFilter(IteratorFilters::LocallyOwnedCell(),
+              magnetic_field->get_dof_handler().end()),
+   worker,
+   copier,
+   AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<dim>(
+     *this->mapping,
+     quadrature_formula,
+     magnetic_field->get_finite_element(),
+     advection_update_flags,
+     *velocity_fe,
+     advection_update_flags),
+   AssemblyData::MagneticInduction::AdvectionMatrix::Copy(
+     magnetic_field->get_finite_element().dofs_per_cell));
+
+  // Compress global data
+  this->diffusion_step_advection_matrix.compress(VectorOperation::add);
+
+  if (parameters.verbose)
+    *this->pcout << " done!" << std::endl;
+}
+
+
+
+template <int dim>
+void MagneticInduction<dim>::assemble_local_advection_matrix(
+  const typename DoFHandler<dim>::active_cell_iterator            &cell,
+  AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<dim>  &scratch,
+  AssemblyData::MagneticInduction::AdvectionMatrix::Copy          &data)
+{
+  // Reset local data
+  data.local_matrix = 0.;
+
+  // Taylor extrapolation coefficients
+  const std::vector<double> eta = this->time_stepping.get_eta();
+
+  // Vector extractor
+  const FEValuesExtractors::Vector  vector_extractor(0);
+
+  // Local to global indices mapping
+  cell->get_dof_indices(data.local_dof_indices);
+
+  // Magnetic field's cell data
+  scratch.magnetic_field_fe_values.reinit(cell);
+
+  // Instantiation of velocity related quantities
+  std::vector<Tensor<1,dim>>  velocity_values(
+                                scratch.n_q_points,
+                                Tensor<1,dim>());
+
+  std::vector<Tensor<2,dim>>  velocity_gradients(
+                                scratch.n_q_points,
+                                Tensor<2,dim>());
+
+  std::vector<double>         velocity_divergences(
+                                scratch.n_q_points,
+                                0.0);
+
+  // Velocity's cell data
+  if (velocity != nullptr)
+  {
+    typename DoFHandler<dim>::active_cell_iterator
+    velocity_cell(&magnetic_field->get_triangulation(),
+                  cell->level(),
+                  cell->index(),
+                  //Pointer to the velocity's DoFHandler
+                  &velocity->get_dof_handler());
+
+    scratch.velocity_fe_values.reinit(velocity_cell);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_values(
+      velocity->old_solution,
+      scratch.old_velocity_values);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_values(
+      velocity->old_old_solution,
+      scratch.old_old_velocity_values);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_gradients(
+      velocity->old_solution,
+      scratch.old_velocity_gradients);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_gradients(
+      velocity->old_old_solution,
+      scratch.old_old_velocity_gradients);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_divergences(
+      velocity->old_solution,
+      scratch.old_velocity_divergences);
+
+    scratch.velocity_fe_values[vector_extractor].get_function_divergences(
+      velocity->old_old_solution,
+      scratch.old_old_velocity_divergences);
+
+    for (unsigned int i = 0; i < scratch.n_q_points; ++i)
+    {
+      scratch.velocity_values[i] =
+        eta[0] * scratch.old_velocity_values[i] +
+        eta[1] * scratch.old_old_velocity_values[i];
+
+      scratch.velocity_gradients[i] =
+        eta[0] * scratch.old_velocity_gradients[i] +
+        eta[1] * scratch.old_old_velocity_gradients[i];
+
+      scratch.velocity_divergences[i] =
+        eta[0] * scratch.old_velocity_divergences[i] +
+        eta[1] * scratch.old_old_velocity_divergences[i];
+    }
+  }
+  else if (ptr_velocity_function != nullptr)
+  {
+    ptr_velocity_function->value_list(
+      scratch.magnetic_field_fe_values.get_quadrature_points(),
+      scratch.velocity_values);
+
+    ptr_velocity_function->gradient_list(
+      scratch.magnetic_field_fe_values.get_quadrature_points(),
+      scratch.velocity_gradients);
+
+    for (unsigned int i = 0; i < scratch.n_q_points; ++i)
+      scratch.velocity_divergences[i] = trace(scratch.velocity_gradients[i]);
+  }
+  else
+  {
+    ZeroTensorFunction<1,dim>().value_list(
+      scratch.magnetic_field_fe_values.get_quadrature_points(),
+      scratch.velocity_values);
+
+    ZeroTensorFunction<2,dim>().value_list(
+      scratch.magnetic_field_fe_values.get_quadrature_points(),
+      scratch.velocity_gradients);
+
+    ZeroFunction<dim>().value_list(
+      scratch.magnetic_field_fe_values.get_quadrature_points(),
+      scratch.velocity_divergences);
+  }
+
+  // Loop over quadrature points
+  for (unsigned int q = 0; q < scratch.n_q_points; ++q)
+  {
+    // Extract test function values at the quadrature points
+    for (unsigned int i = 0; i < scratch.dofs_per_cell; ++i)
+    {
+      scratch.phi[i]      = scratch.magnetic_field_fe_values[vector_extractor].value(i, q);
+      scratch.grad_phi[i] = scratch.magnetic_field_fe_values[vector_extractor].gradient(i, q);
+      scratch.div_phi[i]  = scratch.magnetic_field_fe_values[vector_extractor].divergence(i,q);
+    }
+
+    // Loop over local degrees of freedom
+    for (unsigned int i = 0; i < scratch.dofs_per_cell; ++i)
+      for (unsigned int j = 0; j < scratch.dofs_per_cell; ++j)
+        data.local_matrix(i,j) +=
+          scratch.phi[i] *
+          (scratch.velocity_values[q] *
+           scratch.div_phi[j]
+           -
+           scratch.phi[j] *
+           scratch.velocity_divergences[q]
+           +
+           scratch.velocity_gradients[q] *
+           scratch.phi[j]
+           -
+           scratch.grad_phi[j] *
+           scratch.velocity_values[q]) *
+          scratch.magnetic_field_fe_values.JxW(q);
+    // Loop over local degrees of freedom
+  } // Loop over quadrature points
+}
+
+
+
+template <int dim>
+void MagneticInduction<dim>::copy_local_to_global_advection_matrix(
+  const AssemblyData::MagneticInduction::AdvectionMatrix::Copy  &data)
+{
+  magnetic_field->get_constraints().distribute_local_to_global(
+    data.local_matrix,
+    data.local_dof_indices,
+    this->diffusion_step_advection_matrix);
 }
 
 
@@ -29,3 +259,17 @@ void MagneticInduction<dim>::assemble_advection_matrix()
 // Explicit instantiations
 template void RMHD::Solvers::MagneticInduction<2>::assemble_advection_matrix();
 template void RMHD::Solvers::MagneticInduction<3>::assemble_advection_matrix();
+
+template void RMHD::Solvers::MagneticInduction<2>::assemble_local_advection_matrix
+(const typename DoFHandler<2>::active_cell_iterator                 &,
+ RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<2> &,
+ RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Copy       &);
+template void RMHD::Solvers::MagneticInduction<3>::assemble_local_advection_matrix
+(const typename DoFHandler<3>::active_cell_iterator                 &,
+ RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Scratch<3> &,
+ RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Copy       &);
+
+template void RMHD::Solvers::MagneticInduction<2>::copy_local_to_global_advection_matrix
+(const RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Copy &);
+template void RMHD::Solvers::MagneticInduction<3>::copy_local_to_global_advection_matrix
+(const RMHD::AssemblyData::MagneticInduction::AdvectionMatrix::Copy &);
--- source/magnetic_induction/assembly_projection_step_rhs.cc
+++ source/magnetic_induction/assembly_projection_step_rhs.cc
@@ -1,5 +1,8 @@
 #include <rotatingMHD/magnetic_induction.h>
 
+#include <deal.II/base/work_stream.h>
+#include <deal.II/grid/filtered_iterator.h>
+
 
 
 namespace RMHD
@@ -15,7 +18,140 @@ namespace Solvers
 template <int dim>
 void MagneticInduction<dim>::assemble_projection_step_rhs()
 {
+  if (parameters.verbose)
+    *this->pcout << "  Magnetic induction: Assembling the projection step's right hand side...";
 
+  TimerOutput::Scope  t(*this->computing_timer, "Magnetic induction: Projection step - RHS assembly");
+
+  // Reset data
+  this->projection_step_rhs = 0.;
+
+  // Initiate the quadrature formula for exact numerical integration
+  const QGauss<dim>   quadrature_formula(this->auxiliary_scalar->fe_degree() + 1);
+
+  // Set up the lambda function for the local assembly operation
+  auto worker =
+    [this](const typename DoFHandler<dim>::active_cell_iterator &cell,
+           AssemblyData::MagneticInduction::ProjectionStepRHS::Scratch<dim> &scratch,
+           AssemblyData::MagneticInduction::ProjectionStepRHS::Copy         &data)
+    {
+      this->assemble_local_projection_step_rhs(cell,
+                                               scratch,
+                                               data);
+    };
+
+  // Set up the lambda function for the copy local to global operation
+  auto copier =
+    [this](const AssemblyData::MagneticInduction::ProjectionStepRHS::Copy  &data)
+    {
+      this->copy_local_to_global_projection_step_rhs(data);
+    };
+
+  // Assemble using the WorkStream approach
+  using CellFilter =
+    FilteredIterator<typename DoFHandler<dim>::active_cell_iterator>;
+
+  const UpdateFlags magnetic_field_update_flags =
+    update_gradients;
+
+  const UpdateFlags auxiliary_scalar_update_flags =
+    update_values|
+    update_JxW_values;
+
+  WorkStream::run(
+    CellFilter(IteratorFilters::LocallyOwnedCell(),
+               this->auxiliary_scalar->get_dof_handler().begin_active()),
+    CellFilter(IteratorFilters::LocallyOwnedCell(),
+               this->auxiliary_scalar->get_dof_handler().end()),
+    worker,
+    copier,
+    AssemblyData::MagneticInduction::ProjectionStepRHS::Scratch<dim>(
+      *this->mapping,
+      quadrature_formula,
+      magnetic_field->get_finite_element(),
+      magnetic_field_update_flags,
+      this->auxiliary_scalar->get_finite_element(),
+      auxiliary_scalar_update_flags),
+    AssemblyData::MagneticInduction::ProjectionStepRHS::Copy(
+      this->auxiliary_scalar->get_finite_element().dofs_per_cell));
+
+  // Compress global data
+  this->projection_step_rhs.compress(VectorOperation::add);
+
+  // Compute the L2 norm of the right hand side
+  this->norm_projection_step_rhs = this->projection_step_rhs.l2_norm();
+
+  if (parameters.verbose)
+    *this->pcout << " done!" << std::endl
+                 << "    Right-hand side's L2-norm = "
+                 << std::scientific << std::setprecision(6)
+                 << this->norm_projection_step_rhs
+                 << std::endl;
+}
+
+
+
+template <int dim>
+void MagneticInduction<dim>::assemble_local_projection_step_rhs(
+  const typename DoFHandler<dim>::active_cell_iterator            &cell,
+  AssemblyData::MagneticInduction::ProjectionStepRHS::Scratch<dim> &scratch,
+  AssemblyData::MagneticInduction::ProjectionStepRHS::Copy         &data)
+{
+  // Reset local data
+  data.local_rhs = 0.;
+
+  // VSIMEX coefficient
+  const std::vector<double> alpha = this->time_stepping.get_alpha();
+
+  // Local to global indices mapping
+  cell->get_dof_indices(data.local_dof_indices);
+
+  // Auxiliary scalar
+  scratch.auxiliary_scalar_fe_values.reinit(cell);
+
+  // Magnetic field
+  typename DoFHandler<dim>::active_cell_iterator
+  magnetic_field_cell(&magnetic_field->get_triangulation(),
+                      cell->level(),
+                      cell->index(),
+                      &magnetic_field->get_dof_handler());
+
+  scratch.magnetic_field_fe_values.reinit(magnetic_field_cell);
+
+  const FEValuesExtractors::Vector  vector_extractor(0);
+
+  scratch.magnetic_field_fe_values[vector_extractor].get_function_divergences(
+    magnetic_field->solution,
+    scratch.magnetic_field_divergences);
+
+  // Loop over quadrature points
+  for (unsigned int q = 0; q < scratch.n_q_points; ++q)
+  {
+    // Extract test function values at the quadrature points
+    for (unsigned int i = 0; i < scratch.dofs_per_cell; ++i)
+      scratch.phi[i] = scratch.auxiliary_scalar_fe_values.shape_value(i, q);
+
+    // Loop over local degrees of freedom
+    for (unsigned int i = 0; i < scratch.dofs_per_cell; ++i)
+      data.local_rhs(i) +=
+              alpha[0] / this->time_stepping.get_next_step_size() *
+              scratch.phi[i] *
+              scratch.magnetic_field_divergences[q] *
+              scratch.auxiliary_scalar_fe_values.JxW(q);
+  } // Loop over quadrature points
+}
+
+
+
+template <int dim>
+void MagneticInduction<dim>::copy_local_to_global_projection_step_rhs(
+  const AssemblyData::MagneticInduction::ProjectionStepRHS::Copy &data)
+{
+  this->auxiliary_scalar->get_constraints().distribute_local_to_global(
+    data.local_rhs,
+    data.local_dof_indices,
+    this->projection_step_rhs,
+    data.local_matrix_for_inhomogeneous_bc);
 }
 
 
@@ -29,3 +165,17 @@ void MagneticInduction<dim>::assemble_projection_step_rhs()
 // Explicit instantiations
 template void RMHD::Solvers::MagneticInduction<2>::assemble_projection_step_rhs();
 template void RMHD::Solvers::MagneticInduction<3>::assemble_projection_step_rhs();
+
+template void RMHD::Solvers::MagneticInduction<2>::assemble_local_projection_step_rhs
+(const typename DoFHandler<2>::active_cell_iterator                   &,
+ RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Scratch<2> &,
+ RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Copy       &);
+template void RMHD::Solvers::MagneticInduction<3>::assemble_local_projection_step_rhs
+(const typename DoFHandler<3>::active_cell_iterator                   &,
+ RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Scratch<3> &,
+ RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Copy       &);
+
+template void RMHD::Solvers::MagneticInduction<2>::copy_local_to_global_projection_step_rhs
+(const RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Copy &);
+template void RMHD::Solvers::MagneticInduction<3>::copy_local_to_global_projection_step_rhs
+(const RMHD::AssemblyData::MagneticInduction::ProjectionStepRHS::Copy &);
